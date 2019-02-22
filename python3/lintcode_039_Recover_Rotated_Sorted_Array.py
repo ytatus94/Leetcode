3d4225622c499ec51a3e158b3a26daff6bc62b3b
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 10:43:53 -0800

    Create lintcode_039
    Create lintcode_039_Recover_Rotated_Sorted_Array.py

---
+++ python3/lintcode_039_Recover_Rotated_Sorted_Array.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    @param nums: An integer array
+    @return: nothing
+    """
+    def recoverRotatedSortedArray(self, nums):
+        # write your code here
+        if len(nums) == 0:
+            return
+        
+        # for i in range(len(nums)):
+        #     if nums[i] > nums[i + 1]:
+        #         nums = nums[i+1:] + nums[:i+1] # 這樣子輸出雖然正確，但是不是 in-place
+        #         print(nums)
+        #         break
+        
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                self.swap(nums, 0, i+1)
+                self.swap(nums, i+1, len(nums))
+                self.swap(nums, 0, len(nums))
+                break
+          
+    def swap(self, nums, start, end):  
+        temp = nums[start:end]
+        temp = temp[::-1]
+        for i in range(len(temp)):
+            nums[start + i] = temp[i]
