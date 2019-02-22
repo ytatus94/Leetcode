3f14e03598c490a78f0c19e1da1d1f78cc229728
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 10:41:04 -0800

    Update 162

--- python3/162_Find_Peak_Element.py
+++ python3/162_Find_Peak_Element.py
@@ -21,3 +21,28 @@ class Solution:
         if nums[start] > nums[end]:
             return start
         return end
+
+# lintcode 75
+class Solution:
+    """
+    @param A: An integers array.
+    @return: return any of peek positions.
+    """
+    def findPeak(self, A):
+        # write your code here
+        if len(A) == 0:
+            return -1
+            
+        start = 1
+        end = len(A) - 2
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if A[mid] > A[mid - 1]:
+                start = mid
+            elif A[mid] > A[mid + 1]:
+                end = mid
+            else:
+                end = mid
+        if A[start] > A[end]:
+            return start
+        return end
