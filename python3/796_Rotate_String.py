b60c665da92b0424b71ab4ab94b4359b1cdfe429
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 10:45:05 -0800

    Update 796

--- python3/796_Rotate_String.py
+++ python3/796_Rotate_String.py
@@ -9,3 +9,21 @@ class Solution:
         # 那 B 一定會在 A + A 裡面
         # 例如 ab(cdeab)cde
         return B in A + A and len(B) == len(A)
+
+# lintcode 8
+class Solution:
+    """
+    @param str: An array of char
+    @param offset: An integer
+    @return: nothing
+    """
+    def rotateString(self, str, offset):
+        # write your code here
+        if offset > len(str) and len(str) > 0:
+            offset = offset % len(str)
+            
+        # abcdefgabcdefg
+        temp = (str * 2)[len(str) - offset : 2 * len(str) - offset]
+        
+        for idx in range(len(str)):
+            str[idx] = temp[idx] # 要修改原本的 str
