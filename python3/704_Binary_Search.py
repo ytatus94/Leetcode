b9df0801afae88c424d62973d592aefa81c976f3
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 10:33:27 -0800

    Update 704

--- python3/704_Binary_Search.py
+++ python3/704_Binary_Search.py
@@ -21,3 +21,33 @@ class Solution:
         if nums[tail] == target:
             return tail
         return -1
+
+# lintcode 457
+class Solution:
+    """
+    @param: nums: An integer array sorted in ascending order
+    @param: target: An integer
+    @return: An integer
+    """
+    def findPosition(self, nums, target):
+        # write your code here
+        if len(nums) == 0:
+            return -1
+        
+        start = 0
+        end = len(nums) - 1
+        
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid
+            elif nums[mid] < target:
+                start = mid
+                
+        if nums[start] == target:
+            return start
+        if nums[end] == target:
+            return end
+        return -1
