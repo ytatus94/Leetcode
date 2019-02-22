b055fef1d840f50fec14ce843d1937d404676d5c
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 10:39:59 -0800

    Update 852

--- python3/852_Peak_Index_in_a_Mountain_Array.py
+++ python3/852_Peak_Index_in_a_Mountain_Array.py
@@ -24,3 +24,29 @@ class Solution:
         for i in range(len(A)):
             if A[i] > A[i + 1]:
                 return i
+
+# lintcode 585
+class Solution:
+    """
+    @param nums: a mountain sequence which increase firstly and then decrease
+    @return: then mountain top
+    """
+    def mountainSequence(self, nums):
+        # write your code here
+        if len(nums) == 0:
+            return 0
+            
+        start = 0
+        end = len(nums) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
+                return nums[mid]
+            elif nums[mid] > nums[mid - 1] and nums[mid] < nums[mid + 1]:
+                start = mid
+            elif nums[mid] < nums[mid - 1] and nums[mid] > nums[mid + 1]:
+                end = mid
+        
+        if nums[start] > nums[end]:
+            return nums[start]
+        return nums[end]
