27a1be2159a691f0f7870086b32ea2372c37eedf
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 10:34:49 -0800

    Update 278

--- python3/278_First_Bad_Version.py
+++ python3/278_First_Bad_Version.py
@@ -16,3 +16,25 @@ class Solution:
         if isBadVersion(start):
             return start
         return end
+
+# lintcode 74
+class Solution:
+    """
+    @param n: An integer
+    @return: An integer which is the first bad version.
+    """
+    def findFirstBadVersion(self, n):
+        # write your code here
+        start = 1
+        end = n
+        
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if SVNRepo.isBadVersion(mid) == True:
+                end = mid
+            elif SVNRepo.isBadVersion(mid) == False:
+                start = mid
+                
+        if SVNRepo.isBadVersion(start) == True:
+            return start
+        return end
