749cbbab7b9e4d1746b9123fc266ea6a43ec9198
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 10:37:35 -0800

    Update 240

--- python3/240_Search_a_2D_Matrix_II.py
+++ python3/240_Search_a_2D_Matrix_II.py
@@ -15,3 +15,41 @@ class Solution:
             if target in row:
                 return True
         return False
+
+# lintcode 38
+class Solution:
+    """
+    @param matrix: A list of lists of integers
+    @param target: An integer you want to search in matrix
+    @return: An integer indicate the total occurrence of target in the given matrix
+    """
+    def searchMatrix(self, matrix, target):
+        # write your code here
+        nrows = len(matrix)
+        if nrows == 0:
+            return 0
+            
+        ncols = len(matrix[0])
+        if ncols == 0:
+            return 0
+            
+        count = 0
+        row = 0
+        col = 0
+        while row < nrows and col < ncols:
+            if matrix[row][col] == target:
+                print(row, col)
+                count += 1
+                row += 1
+                col = 0
+                
+            elif matrix[row][col] < target:
+                col += 1
+                if col == ncols:
+                    row += 1
+                    col = 0
+            else:
+                row += 1
+                col = 0
+
+        return count
