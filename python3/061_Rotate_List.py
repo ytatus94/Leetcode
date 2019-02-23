7518a340b37ed1d5d5c877ec6d714ea390041c08
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 19:14:30 -0800

    Update 061

--- python3/061_Rotate_List.py
+++ python3/061_Rotate_List.py
@@ -44,3 +44,38 @@ class Solution:
         tail.next = head
         
         return new_head
+
+# lintcode 170
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: the List
+    @param k: rotate to the right k places
+    @return: the list after rotation
+    """
+    def rotateRight(self, head, k):
+        # write your code here
+        stack = []
+        curr = head
+        while curr:
+            stack.append(curr)
+            curr = curr.next
+            
+        if len(stack) == 0:
+            return head
+            
+        if k > len(stack) and len(stack) > 0:
+            k = k % len(stack)
+        rotate = stack[len(stack)-k:] + stack[:len(stack)-k]
+        
+        for i in range(len(rotate) - 1):
+            rotate[i].next = rotate[i+1]
+        rotate[-1].next = None
+        return rotate[0]
