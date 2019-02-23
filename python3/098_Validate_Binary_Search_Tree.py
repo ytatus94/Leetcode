3913527ecf97925e5faf97fbbd17833208b4c984
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-23 11:20:09 -0800

    Update 098

--- python3/098_Validate_Binary_Search_Tree.py
+++ python3/098_Validate_Binary_Search_Tree.py
@@ -72,45 +72,46 @@ class Solution:
     @return: True if the binary tree is BST, or false
     """
     # divide conquer
-    # def isValidBST(self, root):
-    #     # write your code here
-    #     is_BST, min_val, max_val = self.helper(root)
-    #     return is_BST
+    def isValidBST(self, root):
+        # write your code here
+        is_BST, min_val, max_val = self.helper(root)
+        return is_BST
         
-    # # 傳回以 root 為根的子樹
-    # # 1. 是否是 BST
-    # # 2. 節點的最小值
-    # # 3. 節點的最大值
-    # def helper(self, root):
-    #     if root is None: # 空節點算是 BST
-    #         return True, -1 * sys.maxsize, sys.maxsize
+    # 傳回以 root 為根的子樹
+    # 1. 是否是 BST
+    # 2. 節點的最小值
+    # 3. 節點的最大值
+    def helper(self, root):
+        if root is None: # 空節點算是 BST
+            return True, -1 * sys.maxsize, sys.maxsize
             
-    #     left_BST, left_min, left_max = self.helper(root.left)
-    #     right_BST, right_min, right_max = self.helper(root.right)
+        left_BST, left_min, left_max = self.helper(root.left)
+        right_BST, right_min, right_max = self.helper(root.right)
         
-    #     if not left_BST or not right_BST:
-    #         return False, 0, 0 # 只要一邊不是 BST 就回傳 Fasle 此時 min, max 已經不重要了，用 0 就好
+        if not left_BST or not right_BST:
+            return False, 0, 0 # 只要一邊不是 BST 就回傳 Fasle 此時 min, max 已經不重要了，用 0 就好
             
-    #     # 當左右子樹都是 BST 時仍然要和 root 比較才知道是否整棵樹是 BST
-    #     # 因為空節點，或是只有一個節點時，都會被判斷成 BST
-    #     # 所以又分成了 3 種情形
-    #     # 1. root 是葉子節點
-    #     # 2. 只有左子樹或只有右子樹
-    #     # 3. 左右子樹都存在
-    #     if root.left is None and root.right is None:
-    #         return True, root.val, root.val
-    #     elif root.right is None: # 只有左子樹
-    #         if left_max < root.val:
-    #             return True, left_min, root.val
-    #     elif root.left is None: # 只有右子樹
-    #         if root.val < right_min:
-    #             return True, root.val, right_max
-    #     else: # 左右子樹都不空
-    #         if left_max < root.val and root.val < right_min:
-    #             return True, left_min, right_max
+        # 當左右子樹都是 BST 時仍然要和 root 比較才知道是否整棵樹是 BST
+        # 因為空節點，或是只有一個節點時，都會被判斷成 BST
+        # 所以又分成了 3 種情形
+        # 1. root 是葉子節點
+        # 2. 只有左子樹或只有右子樹
+        # 3. 左右子樹都存在
+        if root.left is None and root.right is None:
+            return True, root.val, root.val
+        elif root.right is None: # 只有左子樹
+            if left_max < root.val:
+                return True, left_min, root.val
+        elif root.left is None: # 只有右子樹
+            if root.val < right_min:
+                return True, root.val, right_max
+        else: # 左右子樹都不空
+            if left_max < root.val and root.val < right_min:
+                return True, left_min, right_max
                 
-    #     return False, 0, 0
-    
+        return False, 0, 0
+
+class Solution:
     # traverse
     def isValidBST(self, root):
         self.is_BST = True
