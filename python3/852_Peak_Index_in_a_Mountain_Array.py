a9e6f8e7de5c4a8aa0185b7737fbdb4b096a9bbf
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-22 22:20:20 -0800

    Update 852

--- python3/852_Peak_Index_in_a_Mountain_Array.py
+++ python3/852_Peak_Index_in_a_Mountain_Array.py
@@ -5,20 +5,22 @@ class Solution:
         :rtype: int
         """
         # 方法一 慢
-        # start = 0
-        # end = len(A) - 1
-        # while start + 1 < end:
-        #     mid = start + (end - start) // 2
-        #     if A[mid - 1] < A[mid]  and A[mid] > A[mid + 1]:
-        #         return mid
-        #     elif A[mid - 1] < A[mid + 1]:
-        #         start = mid
-        #     elif A[mid - 1] > A[mid + 1]:
-        #         end = mid
-        # if A[start] > A[end]:
-        #     return start
-        # return end
-        
+        start = 0
+        end = len(A) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if A[mid - 1] < A[mid]  and A[mid] > A[mid + 1]:
+                return mid
+            elif A[mid - 1] < A[mid + 1]:
+                start = mid
+            elif A[mid - 1] > A[mid + 1]:
+                end = mid
+        if A[start] > A[end]:
+            return start
+        return end
+
+class Solution:
+    def peakIndexInMountainArray(self, A):
         # 方法二
         # 題目說明先增後減了，只要找到後一個比目前小的就是峰值
         for i in range(len(A)):
