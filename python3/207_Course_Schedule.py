e75f1dbfd2731360913412a3a0f25652183f341b
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-23 17:22:39 -0800

    Update 207_Course_Schedule.py

--- python3/207_Course_Schedule.py
+++ python3/207_Course_Schedule.py
@@ -27,48 +27,49 @@ class Solution:
                     q.append(course)
         
         return can_finish == numCourses
+
+class Solution:        
+        if len(prerequisites) == 0:
+            return True
         
-#         if len(prerequisites) == 0:
-#             return True
-        
-#         # 初始化 graph
-#         graph = {}
-#         for pre in prerequisites:
-#             for node in pre:
-#                 graph[node] = set()
-#         # 建立圖  
-#         for pre in prerequisites:
-#             graph[pre[1]].add(pre[0])
+        # 初始化 graph
+        graph = {}
+        for pre in prerequisites:
+            for node in pre:
+                graph[node] = set()
+        # 建立圖  
+        for pre in prerequisites:
+            graph[pre[1]].add(pre[0])
             
-#         # 初始化 indegree
-#         indegree = {}
-#         for node in graph:
-#             indegree[node] = 0
-#         # 賦值
-#         for node in graph:
-#             for neighbor in graph[node]:
-#                 indegree[neighbor] += 1
+        # 初始化 indegree
+        indegree = {}
+        for node in graph:
+            indegree[node] = 0
+        # 賦值
+        for node in graph:
+            for neighbor in graph[node]:
+                indegree[neighbor] += 1
                 
-#         # topological sorting
-#         # 用 BFS
-#         queue = []
-#         # 把所有 indegree = 0 的 node 放到 queue 裡面
-#         for node in graph:
-#             if indegree[node] == 0:
-#                 queue.append(node)
+        # topological sorting
+        # 用 BFS
+        queue = []
+        # 把所有 indegree = 0 的 node 放到 queue 裡面
+        for node in graph:
+            if indegree[node] == 0:
+                queue.append(node)
                 
-#         count = 0
-#         while queue:
-#             node = queue.pop(0)
-#             count += 1
-#             for neighbor in graph[node]:
-#                 indegree[neighbor] -= 1
-#                 if indegree[neighbor] == 0:
-#                     queue.append(neighbor)
+        count = 0
+        while queue:
+            node = queue.pop(0)
+            count += 1
+            for neighbor in graph[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
                     
-#         # 全部都跑完之後，若課程之間有環狀依賴，那 indegree 就不會是 0
-#         # 唯有全部的 indegree 都是 0 的時候才有可能會把全部的課都修完
-#         for k, v in indegree.items():
-#             if v > 0:
-#                 return False
-#         return True
+        # 全部都跑完之後，若課程之間有環狀依賴，那 indegree 就不會是 0
+        # 唯有全部的 indegree 都是 0 的時候才有可能會把全部的課都修完
+        for k, v in indegree.items():
+            if v > 0:
+                return False
+        return True
