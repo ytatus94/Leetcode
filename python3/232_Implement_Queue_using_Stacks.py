0b706ab9733b464622115d506ac64d859f45610e
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-24 11:45:50 -0800

    Update 232_Implement_Queue_using_Stacks.py

--- python3/232_Implement_Queue_using_Stacks.py
+++ python3/232_Implement_Queue_using_Stacks.py
@@ -50,3 +50,64 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+# lintcode 40
+class MyQueue:
+    
+    def __init__(self):
+        # do intialization if necessary
+        self.in_stack = []
+        self.out_stack = []
+    """
+    @param: element: An integer
+    @return: nothing
+    """
+    def push(self, element):
+        # write your code here
+        self.in_stack.append(element)
+    """
+    @return: An integer
+    """
+    def pop(self):
+        # write your code here
+        if len(self.out_stack) == 0:
+            while len(self.in_stack) != 0:
+                self.out_stack.append( self.in_stack.pop() )
+        return self.out_stack.pop()
+    """
+    @return: An integer
+    """
+    def top(self):
+        # write your code here
+        if len(self.out_stack) == 0:
+            while len(self.in_stack) != 0:
+                self.out_stack.append( self.in_stack.pop() )
+        return self.out_stack[-1]
+        
+class MyQueue:
+    # def __init__(self):
+    #     # do intialization if necessary
+    #     self.items = []
+    # """
+    # @param: element: An integer
+    # @return: nothing
+    # """
+    # def push(self, element):
+    #     # write your code here
+    #     self.items.append(element)
+    # """
+    # @return: An integer
+    # """
+    # def pop(self):
+    #     # write your code here
+    #     if len(self.items) == 0:
+    #         return None
+    #     return self.items.pop(0)
+    # """
+    # @return: An integer
+    # """
+    # def top(self):
+    #     # write your code here
+    #     if len(self.items) == 0:
+    #         return None
+    #     return self.items[0]
