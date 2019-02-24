2677e0688d4f931b63f990f8b6a5c0e1d9e8bc5a
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-23 17:35:08 -0800

    Update 200_Number_of_Islands.py

--- python3/200_Number_of_Islands.py
+++ python3/200_Number_of_Islands.py
@@ -45,3 +45,54 @@ class Solution:
                 if grid[neighbor_x][neighbor_y] == '1':
                     queue.append((neighbor_x, neighbor_y))
                     self.visited[neighbor_x][neighbor_y] = True
+
+ # lintcode 433
+class Solution:
+    """
+    @param grid: a boolean 2D matrix
+    @return: an integer
+    """
+    def numIslands(self, grid):
+        # write your code here
+        if grid is None or len(grid) == 0 or len(grid[0]) == 0:
+            return 0
+            
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        island_counts = 0
+        
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col]:
+                    self.bfs(grid, row, col)
+                    island_counts += 1
+                    
+        return island_counts
+        
+    def bfs(self, grid, row, col):
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        # 座標變換數組
+        dx = [1, -1, 0, 0]
+        dy = [0, 0, 1, -1]
+        
+        queue = [(row, col)]
+        grid[row][col] = 0 # 用過的點就標記成 0
+        
+        while queue:
+            origin = queue.pop(0)
+            x = origin[0]
+            y = origin[1]
+            for i in range(4):
+                neighbor_x = x + dx[i]
+                neighbor_y = y + dy[i]
+                neighbor = (neighbor_x, neighbor_y)
+                
+                if not (0 <= neighbor_x < rows and 0 <= neighbor_y < cols):
+                    continue
+                
+                if grid[neighbor_x][neighbor_y]:
+                    queue.append(neighbor)
+                    grid[neighbor_x][neighbor_y] = 0
