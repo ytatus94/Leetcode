b538fd406d34004f4f34225459f45b2aafae33cc
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-24 11:42:44 -0800

    Update 394_Decode_String.py

--- python3/394_Decode_String.py
+++ python3/394_Decode_String.py
@@ -26,3 +26,43 @@ class Solution:
                 curr_record[1] += c # 紀錄 list 內的第二個元素才是字串
 
         return stack[0][1]
+
+# lintcode 575
+class Solution:
+    """
+    @param s: an expression includes numbers, letters and brackets
+    @return: a string
+    """
+    def expressionExpand(self, s):
+        # write your code here
+        if s is None or len(s) == 0:
+            return s
+        
+        stack = []
+        curr_str = ''
+        num = 0
+        
+        # 構成一個 stack = ['str1', 'num2', 'str2', 'num3',...] 的格式
+        # 然後 num_i 會重複 str_i 次
+        
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == '[':
+                stack.append(curr_str) # 數字之前的字串塞入 stack
+                stack.append(num) # 數字塞入 stack
+                # 歸零
+                curr_str = ''
+                num = 0
+            elif ch == ']':
+                # 遇到 ] 的話，表示目前 stack 裡面最後一個元素一定是數字
+                multiplier = stack.pop()
+                # stack 中要維持 str, num, str, num 的格式，如果 pop 掉了最後的
+                # num ，那 num 前面那個 str 也要一併 pop 出來組成新的字串
+                # 因為 num 是對後面的全部 string 做重複
+                prev_str = stack.pop()
+                curr_str = prev_str + multiplier * curr_str
+            else:
+                curr_str += ch
+        
+        return curr_str
