d61c378af5b21fca8447fda77091a064e59734a7
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-23 17:18:21 -0800

    Create 107_Binary_Tree_Level_Order_Traversal_II.py

---
+++ python3/107_Binary_Tree_Level_Order_Traversal_II.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        # 層級遍歷用 BFS
+        self.result = []
+        
+        if root is None:
+            return self.result
+        
+        # 把所有的起點放到 queue
+        queue = [root]
+        # 用 while loop 不斷的把 queue 中的東西 pop 出來
+        while queue:
+            # queue 裡面只是放某一層的
+            # 所以要先把目前這一層的節點放入 result 中
+            self.result.append([node.val for node in queue])
+            
+            # 用目前這一層的節點來找下一層的節點
+            new_queue = [] # 用來存放下一層的節點
+            for n in queue:
+                if n.left:
+                    new_queue.append(n.left)
+                if n.right:
+                    new_queue.append(n.right)
+                    
+            # 再把目前這層改成下一層
+            queue = new_queue
+            
+        # 所以當離開回圈時 result 就存放了每一層的節點，由 root 到 leaf
+        # 因此要把順序顛倒過來 (用 reversed，但是 reversed 不傳回 list)
+        return list(reversed(self.result))
