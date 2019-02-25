ff8bab3b15e112a4edaa4b66da524889955d507f
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-25 13:53:23 -0800

    Create 626_Exchange_Seats.sql

---
+++ SQL/626_Exchange_Seats.sql
@@ -0,0 +1,9 @@
+SELECT (CASE
+            WHEN MOD(id, 2) = 1 AND id = counts THEN id
+            WHEN MOD(id, 2) = 1 AND id != counts THEN id+1
+            ELSE id-1
+        END
+       ) AS id, student
+FROM seat, (SELECT COUNT(*) AS counts
+            FROM seat) AS seat_counts
+ORDER BY id ASC
