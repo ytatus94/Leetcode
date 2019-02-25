a86c743f14e21fccc3760557d84abd55bd2b6e49
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-25 11:52:02 -0800

    Create 570_Managers_with_at_Least_5_Direct_Reports.sql

---
+++ SQL/570_Managers_with_at_Least_5_Direct_Reports.sql
@@ -0,0 +1,14 @@
+SELECT Name
+FROM Employee
+WHERE Id IN (SELECT ManagerId
+             FROM Employee
+             GROUP BY ManagerId
+             HAVING COUNT(*) > 4);
+
+# 方法二:
+SELECT e2.Name
+FROM Employee AS e1
+JOIN Employee AS e2
+ON e1.ManagerId = e2.Id
+GROUP BY e2.Name
+HAVING COUNT(e2.Name) >= 5;
