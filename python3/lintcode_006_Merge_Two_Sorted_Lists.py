d50c821464d42d0b381d97d35eae3e786dc6d1a5
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-25 23:32:55 -0800

    Update and rename 21_Merge_Two_Sorted_Lists.py to lintcode_006_Merge_Two_Sorted_Lists.py

---
+++ python3/lintcode_006_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    @param A: sorted integer array A
+    @param B: sorted integer array B
+    @return: A new sorted integer array
+    """
+    def mergeSortedArray(self, A, B):
+        # write your code here
+        ptr_a = 0
+        ptr_b = 0
+        new_list = []
+        while ptr_a < len(A) and ptr_b < len(B):
+            if A[ptr_a] < B[ptr_b]:
+                new_list.append(A[ptr_a])
+                ptr_a += 1
+            else:
+                new_list.append(B[ptr_b])
+                ptr_b += 1
+        
+        while ptr_a < len(A):
+            new_list.append(A[ptr_a])
+            ptr_a += 1
+            
+        while ptr_b < len(B):
+            new_list.append(B[ptr_b])
+            ptr_b += 1
+            
+        return new_list
