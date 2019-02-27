14950d2a4256bd4a5601e536f9204e7b50240a47
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-27 15:38:09 -0800

    Create lintcode_Two_Sum_Greater_than_target.py

---
+++ python3/lintcode_Two_Sum_Greater_than_target.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum2(nums, target):
+        if nums is None or len(nums) < 2:
+            return None
+            
+        nums = sorted(nums)
+        
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        
+        while left < right:
+            if nums[left] + nums[right] <= target:
+                left += 1
+            else:
+                count = right - left
+                right -= 1
+                
+        return count
