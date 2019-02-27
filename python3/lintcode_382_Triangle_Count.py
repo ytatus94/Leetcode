740f46db4cb0236ff366d42b324d42f0f561d24c
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-27 15:26:47 -0800

    Create lintcode_382_Triangle_Count.py

---
+++ python3/lintcode_382_Triangle_Count.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param S: A list of integers
+    @return: An integer
+    """
+    def triangleCount(self, S):
+        # write your code here
+        S = sorted(S)
+        res = 0
+        for i in range(len(S)):
+            left = 0
+            right = i - 1
+            while left < right:
+                if S[left] + S[right] > S[i]:
+                    res += (right - left)
+                    right -= 1
+                else:
+                    left += 1
+                    
+        return res
