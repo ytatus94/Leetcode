691ba2fba9deda3d90aec60ed7b8ea43751f407e
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-27 11:54:46 -0800

    Create lintcode_Two_Sum_Unique_pairs.py

---
+++ python3/lintcode_Two_Sum_Unique_pairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(nums, target):
+        if nums is None or len(nums) < 2:
+            return 0
+            
+        nums = sorted(nums)
+        
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        
+        while left < right:
+            if nums[left] + nums[right] == target:
+                count += 1
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+            elif nums[left] + nums[right] < target:
+                left += 1
+            else:
+                right -= 1
+                
+        return count
