4ead65c2ad75f929837db432a50738fe9d44a64c
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-27 15:41:20 -0800

    Create lintcode_Two_Sum_Less_than_or_equal_to_target.py

---
+++ python3/lintcode_Two_Sum_Less_than_or_equal_to_target.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum2(nums, target):
+        if nums is None or len(nums) < 2:
+            return None
+            
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        
+        while left < right:
+            if nums[left] + nums[right] <= target:
+                count += right - left
+                left += 1
+            else:
+                right -= 1
+                
+        return count
