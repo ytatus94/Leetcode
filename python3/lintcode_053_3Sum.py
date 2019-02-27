2a3e31277601b0c638fba58dfb9d3231c4cd6539
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-27 15:06:20 -0800

    Create lintcode_053_3Sum.py

---
+++ python3/lintcode_053_3Sum.py
@@ -0,0 +1,45 @@
+# lintcode 053
+class Solution:
+    """
+    @param numbers: Give an array numbers of n integer
+    @return: Find all unique triplets in the array which gives the sum of zero.
+    """
+    def threeSum(self, numbers):
+        # write your code here
+        if numbers is None or len(numbers) < 3:
+            return None
+            
+        res = []
+        head = 0
+        tail = 2
+        nums = sorted(numbers)
+        
+        for i in range(0, len(nums) - 2):
+            # 重複的數要刪除掉
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            subarray = nums[:i]
+            target = -1 * nums[i]
+            start = i + 1
+            end = len(nums) - 1
+            res += self.two_sum(nums, target, start, end)
+            
+        return res
+        
+    def two_sum(self, nums, target, start, end):
+        result = []
+        while start < end:
+            if nums[start] + nums[end] == target:
+                result.append([-1 * target, nums[start], nums[end]])
+                start += 1
+                end -= 1
+                while start < end and nums[start] == nums[start - 1]:
+                    start += 1
+                while start < end and nums[end] == nums[end + 1]:
+                    end -= 1
+            elif nums[start] + nums[end] < target:
+                start += 1
+            else:
+                end -= 1
+        return result
