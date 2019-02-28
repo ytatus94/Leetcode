ff35cee0159d4b88566addd0dd71fd96efc3a361
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-27 17:56:56 -0800

    Create lintcode_031_Partition_Array.py

---
+++ python3/lintcode_031_Partition_Array.py
@@ -0,0 +1,36 @@
+class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+    def partitionArray(self, nums, k):
+        # write your code here
+        if nums is None or len(nums) == 0:
+            return 0
+
+        nums = sorted(nums)
+
+        for i in range(len(nums)):
+            if nums[i] >= k:
+                return i
+        return len(nums)
+        
+    def partitionArray(self, nums, k):
+        if nums is None or len(nums) == 0:
+            return 0
+
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            # 當 start 和 end 所代表的元素滿足 < k 和 >= k 的要求時
+            # 只是往下/往前移動一個
+            while start <= end and nums[start] < k:
+                start += 1
+            while start <= end and nums[end] >= k:
+                end -= 1
+            if start <= end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        return start
