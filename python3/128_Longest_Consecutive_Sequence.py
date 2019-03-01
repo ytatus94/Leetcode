e3dca26c210573f8792cdc369706a201daf2b157
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-28 21:06:01 -0800

    Create 128_Longest_Consecutive_Sequence.py

---
+++ python3/128_Longest_Consecutive_Sequence.py
@@ -0,0 +1,42 @@
+# lintcode 124
+class Solution:
+    """
+    @param num: A list of integers
+    @return: An integer
+    """
+    def longestConsecutive(self, num):
+        # write your code here
+        if num is None or len(num) == 0:
+            return 0
+            
+        # 當只有一個數的時候，最長序列就是 1
+        if len(num) == 1:
+            return 1
+        
+        prefix_diff = []
+        
+        # 先去重，然後排序
+        num = sorted(set(num))
+        
+        # 計算相鄰兩個數的差
+        for i in range(1, len(num)):
+            prefix_diff.append(num[i] - num[i - 1])
+        
+        longest = 0
+        count = 0
+        for i in range(len(prefix_diff)):
+            if prefix_diff[i] == 1:
+                count += 1
+            else:
+                # 如果 prefix_diff 中有一個數 != 1
+                # 那就把現在的 count 和 longest 做比較，留下最長的，然後把 count 歸零
+                longest = max(longest, count)
+                count = 0
+        
+        # 如果 prefix_diff 裡面全都是 1 ，那上面的 else 就不會執行
+        # 因此需要這一行來計算 count
+        longest = max(longest, count)
+        
+        # 因為 prefix_diff 是相鄰兩數的差
+        # 有 n 組相鄰兩樹的差=1 表示有 n+1 個數，所以要加 1
+        return longest + 1
