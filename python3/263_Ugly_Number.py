ed34cc36a37c597a070d9338bebca83b3d87231a
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-01 10:37:23 -0800

    Update 263_Ugly_Number.py

--- python3/263_Ugly_Number.py
+++ python3/263_Ugly_Number.py
@@ -1,6 +1,6 @@
-class Solution:
+ class Solution:
     def isUgly(self, num: int) -> bool:
-        if num == 0:
+        if num <= 0:
             return False
         
         while num > 1:
@@ -27,7 +27,7 @@ class Solution:
     
         # ugly number 的因數只包含 2, 3, 5
         # 因為 5 最大就先除以 5，讓 num 變小
-        while num >= 5 and num % 5 == 0:
+        while num >= 5 and nclassum % 5 == 0:
             num //= 5
         # 離開迴圈時 num 可能比 5 小，或是非 5 的倍數
         # 但仍可能是 2 或 3 的倍數
