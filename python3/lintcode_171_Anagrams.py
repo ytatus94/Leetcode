5bff9bee8a51873ad8f93fb96e6cc9c0a05a1dc4
Yu-Ting <ytatus94@yahoo.com.tw>
2019-02-28 19:13:30 -0800

    Create lintcode_171_Anagrams.py

---
+++ python3/lintcode_171_Anagrams.py
@@ -0,0 +1,24 @@
+class Solution:
+    """
+    @param strs: A list of strings
+    @return: A list of strings
+    """
+    def anagrams(self, strs):
+        # write your code here
+        if strs is None:
+            return None
+        
+        res = []
+        hash = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word not in hash:
+                hash[sorted_word] = [word]
+            else:
+                hash[sorted_word].append(word)
+                
+        for key, val in hash.items():
+            if len(val) >= 2:
+                res += val
+        
+        return res
