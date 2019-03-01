4800c9b23d5343cbe80377ad0f2f3840a0c656ab
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-01 10:33:49 -0800

    Update 263_Ugly_Number.py

--- python3/263_Ugly_Number.py
+++ python3/263_Ugly_Number.py
@@ -1,3 +1,21 @@
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        if num == 0:
+            return False
+        
+        while num > 1:
+            if num % 2 == 0:
+                num //= 2
+            elif num % 3 == 0:
+                num //= 3
+            elif num % 5 == 0:
+                num //= 5
+            else:
+                return False
+        # 輸入的 num 如果是 ugly number
+        # 離開迴圈時 num 應該是 1
+        return True
+
 # lintcode 517
 class Solution:
     """
