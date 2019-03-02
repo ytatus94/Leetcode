27ea30cb5d4a943d32fbec65078fe9578c0d656b
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-01 20:35:57 -0800

    Create lintcode_544_Top_k_Largest_Numbers.py

---
+++ python3/lintcode_544_Top_k_Largest_Numbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    @param nums: an integer array
+    @param k: An integer
+    @return: the top k largest numbers in array
+    """
+    def topk(self, nums, k):
+        # write your code here
+        return sorted(nums, reverse=True)[:k]
