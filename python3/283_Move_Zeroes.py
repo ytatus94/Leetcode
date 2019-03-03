cdda66af1e032b4e306b2b00e1b23bb420ec56f7
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-03 09:26:35 -0800

    Update 283_Move_Zeroes.py

--- python3/283_Move_Zeroes.py
+++ python3/283_Move_Zeroes.py
@@ -27,3 +27,22 @@ class Solution:
             if nums[j] != 0:
                 j += 1
             i += 1
+
+class Solution:
+    """
+    @param nums: an integer array
+    @return: nothing
+    """
+    def moveZeroes(self, nums):
+        # write your code here
+        i = 0
+        j = 0
+        while i < len(nums):
+            if nums[i] != 0 and nums[j] == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+            # 把 j 移到下一個 0 所在位置
+            # 注意 j 要小於 i 不然會出錯
+            # 例如 [5,4,3,2,1] 中都沒有 0 所以當 i = 0 時，j 會移動到 j=5 就超出了 len(nums) 的長度
+            while j < i and nums[j] != 0:
+                j += 1
+            i += 1
