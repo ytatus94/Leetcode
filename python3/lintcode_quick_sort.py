b8d32fc290a4810f7ae0d89f07a307f0bd9018e7
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-04 10:18:49 -0800

    Create lintcode_quick_sort.py

---
+++ python3/lintcode_quick_sort.py
@@ -0,0 +1,22 @@
+class Solution:
+    def quick_sort(self, nums, start, end):
+        if start >= end:
+            return
+            
+        left = start
+        right = left
+        pivot = nums[(start + end) // 2] # pivot 用的是值，不是用 index
+        
+        # 比較左右時一定要用 left <= right，不是用 left < right
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1
+            while left <= right and nums[right] > pivot:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+                
+        self.quick_sort(nums, start, right)
+        self.quick_sort(nums, left, end)
