71622418777eedd8fa8f9283d5cbcb11bbd684e9
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-06 21:10:36 -0800

    Update 263_Ugly_Number.py

--- python3/263_Ugly_Number.py
+++ python3/263_Ugly_Number.py
@@ -45,3 +45,16 @@ class Solution:
         if num == 1:
             return True
         return False
+
+class Solution:
+    def isUgly(self, num):
+        if num > 1:
+            while num % 2 == 0:
+                num //= 2
+            while num % 3 == 0:
+                num //= 3
+            while num % 5 == 0:
+                num //= 5
+        if num == 1:
+            return True
+        return False
