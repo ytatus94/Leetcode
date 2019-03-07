8e1ff0dba522e1cf01c47ebf0e0836d7df971358
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-07 12:05:33 -0800

    Update 023_Merge_k_Sorted_Lists.py

--- python3/023_Merge_k_Sorted_Lists.py
+++ python3/023_Merge_k_Sorted_Lists.py
@@ -1,3 +1,65 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeKLists(self, lists):
+        if lists is None or len(lists) == 0:
+            return None
+        # bottom_up = self.merge_helper_bottom_up(lists, 0, len(lists) - 1)
+        # return bottom_up
+    
+        top_down = self.merge_helper_top_down(lists, 0, len(lists) - 1)
+        return top_down
+
+    def merge_helper_bottom_up(self, lists, start, end):
+        if start == end:
+            return lists[start]
+
+        mid = (start + end) // 2
+        left = self.merge_helper_bottom_up(lists, start, mid)
+        right = self.merge_helper_bottom_up(lists, mid + 1, end)
+        return self.merge_two_sorted_lists(left, right)
+
+    def merge_two_sorted_lists(self, list1, list2):
+        if list1 is None and list2 is None:
+            return None
+        elif list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+
+        dummy = ListNode(0)
+        curr = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        return dummy.next
+
+    def merge_helper_top_down(self, lists, start, end):
+        while len(lists) > 1:
+            merged_list = []
+            start = 0
+            end = len(lists) - 1
+            while start < end:
+                merged_list.append(self.merge_two_sorted_lists(lists[start], lists[start + 1]))
+                start += 2
+            if (end + 1) % 2 == 1:
+                merged_list.append(lists[-1])
+            lists = merged_list
+        return lists[0]
+    
 # lintcode 104
 """
 Definition of ListNode
