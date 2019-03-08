9a7948bcd3e933cb1977776af824fe224152a6c0
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-08 09:24:05 -0800

    Update 771_Jewels_and_Stones.py

--- python3/771_Jewels_and_Stones.py
+++ python3/771_Jewels_and_Stones.py
@@ -5,3 +5,13 @@ class Solution:
             if i in J:
                 count += 1
         return count
+
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        jewels = [i for i in S if i in J]
+        return len(jewels)
