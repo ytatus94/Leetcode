edd94e019b91b4ff48ce989a0497508069234059
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-07 17:49:58 -0800

    Create 942_DI_String_Match.py

---
+++ python3/942_DI_String_Match.py
@@ -0,0 +1,13 @@
+class Solution:
+    def diStringMatch(self, S: 'str') -> 'List[int]':
+        N = len(S)
+        A = [i for i in range(N + 1)]
+        res = []
+        for i in range(N):
+            if S[i] == 'I':
+                res.append(A.pop(0))
+            else:
+                res.append(A.pop())
+        res.append(A.pop())
+        print(res)
+        return res
