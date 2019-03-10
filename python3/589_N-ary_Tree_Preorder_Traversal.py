9be6bd945861b5332bda5f7dec64191380d6c8e0
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-09 16:25:34 -0800

    Update 589_N-ary_Tree_Preorder_Traversal.py

--- python3/589_N-ary_Tree_Preorder_Traversal.py
+++ python3/589_N-ary_Tree_Preorder_Traversal.py
@@ -1,3 +1,10 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
 class Solution:
     def preorder(self, root):
         """
