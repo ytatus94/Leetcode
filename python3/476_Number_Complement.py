48a7c1e377e78f8fef558ae71577506efe269ef8
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-10 20:10:37 -0700

    Update 476_Number_Complement.py

--- python3/476_Number_Complement.py
+++ python3/476_Number_Complement.py
@@ -15,3 +15,15 @@ class Solution:
         for i in remainder:
             complement = complement * 2 + i
         return complement
+
+class Solution:
+        # bin() 會傳回 0b 開頭的二進位，所以要從 index=2 開始才是二進位的數值
+        # bin() 傳回的是 str 型態
+        binary = bin(num)[2:]
+        complement = ''
+        for i in range(len(binary)):
+            if binary[i] == '0': # 注意是 str 的型態
+                complement += '1'
+            else:
+                complement += '0'
+        return int(complement, 2) # 第二個參數是第一個參數的 base
