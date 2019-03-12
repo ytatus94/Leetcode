1adb35e333eb4a3155d3f4d942809b9368379e16
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-12 11:46:55 -0700

    Create 429_N-ary_Tree_Level_Order_Traversal.py

---
+++ python3/429_N-ary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        res = []
+        if root is None:
+            return res
+        
+        queue = [root]
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                for child in node.children:
+                    queue.append(child)
+            res.append(level)
+            
+        return res
