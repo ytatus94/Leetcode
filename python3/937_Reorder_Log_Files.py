3b3804b63ee5c46ef9a7b00d24eeebff5c8256be
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-11 23:12:47 -0700

    Update 937_Reorder_Log_Files.py

--- python3/937_Reorder_Log_Files.py
+++ python3/937_Reorder_Log_Files.py
@@ -12,9 +12,12 @@ class Solution:
             else:
                 letter.append(log)
                 
-        # 排序 letter-logs
-        # 排序時，照文字順序排序，不考慮 identifier
-        # 但是當文字都一樣的時候，就用 identifier 排序
+        '''
+        排序 letter-logs
+        排序時，照文字順序排序，不考慮 identifier
+        但是當文字都一樣的時候，就用 identifier 排序
+        所以 identifier 排序要先執行
+        '''
         letter.sort(key=lambda x: x.split()[0]) # 用 identifier 排序
         letter.sort(key=lambda x: x.split()[1:]) # 用文字排序
         
