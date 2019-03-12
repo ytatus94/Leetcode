6d5174fa6fb5007458bcdc17b2110c2eda2d2606
Yu-Ting <ytatus94@yahoo.com.tw>
2019-03-11 23:06:53 -0700

    Create 937_Reorder_Log_Files.py

---
+++ python3/937_Reorder_Log_Files.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        if logs is None or len(logs) == 0:
+            return logs
+
+        # 先把 logs 分成 letter-logs 和 digit-logs
+        digit = []
+        letter = []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digit.append(log)
+            else:
+                letter.append(log)
+                
+        # 排序 letter-logs
+        # 排序時，照文字順序排序，不考慮 identifier
+        # 但是當文字都一樣的時候，就用 identifier 排序
+        letter.sort(key=lambda x: x.split()[0]) # 用 identifier 排序
+        letter.sort(key=lambda x: x.split()[1:]) # 用文字排序
+        
+        return letter + digit
