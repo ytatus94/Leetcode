23e28a9b1d5eb401c24e4b4a4319596767db3c17
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-01-18 22:40:41 -0800

    Update 090_Subsets_II.py

--- python3/090_Subsets_II.py
+++ python3/090_Subsets_II.py
@@ -22,3 +22,28 @@ class Solution:
             if i > index and nums[i] == nums[i - 1]: # 這邊在去重
                 continue
             self.dfs(nums, results, subset + [nums[i]], i + 1)
+
+# 方法2:
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        if nums is None:
+            return []
+        
+        results = []
+        # nums 有重複的值，所以要先排序
+        # sorted(list) 會傳回新的 list
+        # list.sort() 則是 inplace
+        sorted_nums = sorted(nums)
+        dfs(results, [], sorted_nums)
+        return results
+    
+def dfs(results, subset, rest):
+    results.append(subset)
+    for i in range(len(rest)):
+        # 如果目前循環到的數 rest[i] 和前一個數 rest[i-1] 一樣
+        # 那就跳過目前的循環，才不會產生重複的 subset
+        # 但是當 i = 0 時，i -1 = -1 會變成取出 rest[-1]
+        # 這反而不對了，所以要避開 i = 0 的狀況
+        if i != 0 and rest[i] == rest[i - 1]:
+            continue
+        dfs(results, subset + [rest[i]], rest[i+1:])
