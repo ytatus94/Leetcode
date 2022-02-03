a93f93fac671ccec80811756d9e2eeac2e3baaec
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-02-02 21:51:37 -0800

    Create lintcode_222_Setter_and_Getter.py

---
+++ python3/lintcode_222_Setter_and_Getter.py
@@ -0,0 +1,15 @@
+class School:
+    '''
+     * Declare a setter method `setName` which expect a parameter *name*.
+    '''
+    # write your code here
+    def setName(self, name):
+        self.__name = name
+
+    '''
+     * Declare a getter method `getName` which expect no parameter and return
+     * the name of this school
+    '''
+    # write your code here
+    def getName(self):
+        return self.__name
