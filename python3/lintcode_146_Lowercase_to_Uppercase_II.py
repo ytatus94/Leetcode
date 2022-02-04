b2f0ef47e7a7c6ff03e0407a6ef16f67c0e93756
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-02-04 09:27:41 -0800

    Create lintcode_146_Lowercase_to_Uppercase_II.py

---
+++ python3/lintcode_146_Lowercase_to_Uppercase_II.py
@@ -0,0 +1,8 @@
+class Solution:
+    """
+    @param letters: A string
+    @return: A string
+    """
+    def lowercaseToUppercase2(self, letters: str) -> str:
+        # write your code here
+        return letters.upper()
