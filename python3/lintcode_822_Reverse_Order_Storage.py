ec8f77b72f328734250f2ff0f834e5305002b08b
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-02-06 00:22:59 -0800

    Create lintcode_822_Reverse_Order_Storage.py

---
+++ python3/lintcode_822_Reverse_Order_Storage.py
@@ -0,0 +1,20 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: the given linked list
+    @return: the array that store the values in reverse order 
+    """
+    def reverseStore(self, head):
+        # write your code here
+        res = []
+        while head is not None:
+            res = [head.val] + res
+            head = head.next
+        return res
