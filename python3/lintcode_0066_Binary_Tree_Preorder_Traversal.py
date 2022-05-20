1bdf6292481ea8804369b9827c5709e2d2284e1a
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-05-20 14:54:35 -0700

    Create lintcode_0066_Binary_Tree_Preorder_Traversal.py

---
+++ python3/lintcode_0066_Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,36 @@
+// 用 Traversal 的寫法
+from typing import (
+    List,
+)
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorder_traversal(self, root: TreeNode) -> List[int]:
+        # write your code here
+        result = []
+        self.traversal(root, result) // traversal 是把結果當參數傳遞
+        return result
+        
+    // 把以 root 為根的 preorder 放到 result 裡面去
+    def traversal(self, root, result):
+        // 遞歸的出口: 處理空節點
+        // 空節點時 ==> 什麼都不做
+        if root is None:
+            return
+        result.append(root.val)
+        self.traversal(root.left, result)
+        self.traversal(root.right, result)
