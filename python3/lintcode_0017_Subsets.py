80b5ff7f0e94d5a570fd7fc72790a1e26fbf5681
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-05-20 16:11:51 -0700

    Create lintcode_0017_Subsets.py

---
+++ python3/lintcode_0017_Subsets.py
@@ -0,0 +1,33 @@
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+             we will sort your return value in output
+    """
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # write your code here
+        if nums is None:
+            return []
+        
+        result = []
+        nums = sorted(nums)
+        self.dfs(nums, 0, [], result)
+        return result
+
+    # 定義: 傳回以 current 開頭的所有子集
+    def dfs(self, nums, start_index, current, result):
+        # 出口:
+        if start_index == len(nums):
+            result.append(current.copy())
+            return
+        # 拆解
+        current.append(nums[start_index]) # 要放入 nums[start_index]
+        self.dfs(nums, start_index + 1, current, result)
+
+        current.pop() # 不要放入 nums[start_index]
+        self.dfs(nums, start_index + 1, current, result)
+
