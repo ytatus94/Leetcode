6489c389e6a52dad0b32948f1fbe7b5fcd94fd9c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-05-20 23:14:28 -0700

    Update lintcode_0097_Maximum_Depth_of_Binary_Tree.py

--- python3/lintcode_0097_Maximum_Depth_of_Binary_Tree.py
+++ python3/lintcode_0097_Maximum_Depth_of_Binary_Tree.py
@@ -18,15 +18,17 @@ class Solution:
     def max_depth(self, root: TreeNode) -> int:
         # write your code here
         self.max_depth = 0
-        self.traversal(root, 1)
+        self.traversal(root, 1) # 當前深度是 1
         return self.max_depth
 
+    # 定義: 把以 root 為根的最大深度記錄下來
     def traversal(self, root, depth):
+        # 出口: 處理空節點
         if root is None:
             return
         if self.max_depth < depth:
             self.max_depth = depth
-
+        # 拆解:
         self.traversal(root.left, depth + 1)
         self.traversal(root.right, depth + 1)
 
@@ -39,12 +41,13 @@ class Solution:
     # 定義: 傳回以 root 為根的子樹的最大深度
     def max_depth(self, root: TreeNode) -> int:
         depth = 0
-        # 出口:
+        # 出口: 處理空節點
         if root is None:
             return depth
         # 拆解:
         left_max_depth = self.max_depth(root.left)
         right_max_depth = self.max_depth(root.right)
+        # 合併:
         depth = max(left_max_depth, right_max_depth) + 1
         return depth
 
