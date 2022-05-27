34e34c90753fd6b3eabc8d94441839edad67e5c5
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-05-27 16:41:37 -0700

    Create lintcode_0585_Maximum_Number_in_Mountain_Sequence.py

---
+++ python3/lintcode_0585_Maximum_Number_in_Mountain_Sequence.py
@@ -0,0 +1,12 @@
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: a mountain sequence which increase firstly and then decrease
+    @return: then mountain top
+    """
+    def mountain_sequence(self, nums: List[int]) -> int:
+        # write your code here
+        return max(nums)
