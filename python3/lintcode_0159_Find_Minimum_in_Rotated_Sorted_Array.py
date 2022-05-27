147c69bb93972d4f95382cd6894b209aedc89aa3
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-05-26 17:49:54 -0700

    Update lintcode_0159_Find_Minimum_in_Rotated_Sorted_Array.py

--- python3/lintcode_0159_Find_Minimum_in_Rotated_Sorted_Array.py
+++ python3/lintcode_0159_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -14,3 +14,31 @@ class Solution:
             if i < min_val:
                 min_val = i
         return min_val
+
+    from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: a rotated sorted array
+    @return: the minimum number in the array
+    """
+    def find_min(self, nums: List[int]) -> int:
+        # write your code here
+        target = nums[-1]
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return nums[mid]
+            elif nums[mid] < target:
+                end = mid
+            else:
+                start = mid
+
+        print("out:", start, end)
+        if nums[start] < nums[end]:
+            return nums[start]
+        else:
+            return nums[end]
