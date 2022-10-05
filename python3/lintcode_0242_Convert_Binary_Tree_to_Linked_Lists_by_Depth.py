b5cfe09b858205a075d16eb3880ee151586720c5
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-10-05 12:11:12 -0700

    Update lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py

--- python3/lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py
+++ python3/lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py
@@ -44,3 +44,46 @@ class Solution:
             curr.next = new_node
             curr = curr.next
         return dummy.next
+
+# 方法2: 比較快
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+"""
+class Solution:
+    # @param {TreeNode} root the root of binary tree
+    # @return {ListNode[]} a lists of linked list
+    def binaryTreeToLists(self, root):
+        # Write your code here
+        results = []
+
+        if root is None:
+            return results
+
+        queue = [root]
+
+        dummy = ListNode(0, None)
+        curr = None
+
+        while queue:
+            curr = dummy
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                curr.next = ListNode(node.val, None)
+                curr = curr.next
+
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            results.append(dummy.next)
+
+        return results
