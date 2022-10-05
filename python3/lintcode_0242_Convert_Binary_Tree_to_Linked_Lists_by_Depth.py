3f7985ca3951b4701e7017494dd9ba55e1174f8d
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-10-05 12:05:47 -0700

    Create lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py

---
+++ python3/lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py
@@ -0,0 +1,46 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+"""
+class Solution:
+    # @param {TreeNode} root the root of binary tree
+    # @return {ListNode[]} a lists of linked list
+    def binaryTreeToLists(self, root):
+        # Write your code here
+        results = []
+        if root is None:
+            return results
+
+        queue = [root]
+
+        while queue:
+            curr_level = []
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                curr_level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            print(curr_level)
+            head = self.list_to_linkedlist(curr_level)
+            results.append(head)
+
+        return results
+
+    def list_to_linkedlist(self, nums):
+        dummy = ListNode(0, None)
+        curr = dummy
+        for i in range(len(nums)):
+            new_node = ListNode(nums[i], None)
+            curr.next = new_node
+            curr = curr.next
+        return dummy.next
