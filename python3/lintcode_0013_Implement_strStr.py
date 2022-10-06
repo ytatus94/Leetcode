374b20bccffe2bf71506501f20458e0b96535978
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-10-05 21:37:14 -0700

    Update lintcode_0013_Implement_strStr.py

--- python3/lintcode_0013_Implement_strStr.py
+++ python3/lintcode_0013_Implement_strStr.py
@@ -10,3 +10,15 @@ class Solution:
             return source.index(target)
         else:
             return -1
+
+# 方法2
+# find() 傳回 index，如果找不到就會傳回 -1
+class Solution:
+    """
+    @param source: 
+    @param target: 
+    @return: return the index
+    """
+    def str_str(self, source: str, target: str) -> int:
+        # Write your code here
+        return source.find(target)
