5d6306830c7e51081a020c83070f04975e7d9981
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-10-11 00:08:27 -0700

    Create lintcode_0070_Binary_Tree_Level_Order_Traversal_II.py

---
+++ python3/lintcode_0070_Binary_Tree_Level_Order_Traversal_II.py
@@ -0,0 +1,75 @@
+from typing import (
+    List,
+)
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A tree
+    @return: buttom-up level order a list of lists of integer
+    """
+    def levelOrderBottom(self, root):
+        # write your code here
+        results = []
+        if root is None:
+            return results
+
+        queue = [root]
+        while queue:
+            curr_level = []
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                curr_level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            results.append(curr_level)
+            
+        return list(reversed(results))
+
+# 方法 2:
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A tree
+    @return: buttom-up level order a list of lists of integer
+    """
+    def level_order_bottom(self, root: TreeNode) -> List[List[int]]:
+        # write your code here
+        if root is None:
+            return []
+
+        results = []
+        queue = [root] # 把所有的起點加到 queue 中
+        while queue:
+            # 把現在這一層所有的點加入到 results
+            curr_layer = [node.val for node in queue]
+            results.append(curr_layer)
+            # 把下一層加入到 queue 中
+            next_layer = []
+            for node in queue:
+                if node.left:
+                    next_layer.append(node.left)
+                if node.right:
+                    next_layer.append(node.right)
+            queue = next_layer
+
+        return list(reversed(results))
