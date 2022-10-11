3e052d571552397c26af6a49f4f7008aa9cea3e4
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-10-11 00:42:48 -0700

    Update lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py

--- python3/lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py
+++ python3/lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py
@@ -87,3 +87,49 @@ class Solution:
             results.append(dummy.next)
 
         return results
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+"""
+class Solution:
+    # @param {TreeNode} root the root of binary tree
+    # @return {ListNode[]} a lists of linked list
+    def binaryTreeToLists(self, root):
+        # Write your code here
+        if root is None:
+            return []
+
+        results = []
+        queue = [root]
+        while queue:
+            # 現在這層的節點
+            curr_layer = [node.val for node in queue]
+            print(curr_layer)
+            head = self.convert_list_to_linked_list(curr_layer)
+            results.append(head)
+            # 把下一層的結點加入到 queue 中
+            next_layer = []
+            for node in queue:
+                if node.left:
+                    next_layer.append(node.left)
+                if node.right:
+                    next_layer.append(node.right)
+            queue = next_layer
+        return results
+
+    def convert_list_to_linked_list(self, nums):
+        dummy_node = ListNode(0, None)
+        curr = dummy_node
+        for i in nums:
+            curr.next = ListNode(i, None)
+            curr = curr.next
+        return dummy_node.next
