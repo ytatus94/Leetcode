49a8616fb86d5e2b923c95edaf3e2596d8cd279b
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-10-26 09:02:24 -0700

    Update lintcode_0511_Swap_Two_Nodes_in_Linked_List.py

--- python3/lintcode_0511_Swap_Two_Nodes_in_Linked_List.py
+++ python3/lintcode_0511_Swap_Two_Nodes_in_Linked_List.py
@@ -1,3 +1,4 @@
+# 方法1: 找出兩個節點後更改值
 # lintcode 511
 """
 Definition of ListNode
@@ -32,3 +33,55 @@ class Solution:
             node_v2.val = v1
         
         return head
+
+# 方法2: 題目說要改點，而不是直接改值
+from lintcode import (
+    ListNode,
+)
+
+"""
+Definition of ListNode:
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: a ListNode
+    @param v1: An integer
+    @param v2: An integer
+    @return: a new head of singly-linked list
+    """
+    def swap_nodes(self, head: ListNode, v1: int, v2: int) -> ListNode:
+        # write your code here
+        if head is None:
+            return None
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        curr = dummy
+        prev_n1, prev_n2 = None, None
+        while curr:
+            if curr.next and curr.next.val == v1:
+                prev_n1 = curr
+            if curr.next and curr.next.val == v2:
+                prev_n2 = curr
+            curr = curr.next
+
+        n1, n2 = None, None
+        if prev_n1 and prev_n1.next:
+            n1 = prev_n1.next
+
+        if prev_n2 and prev_n2.next:
+            n2 = prev_n2.next
+
+        if n1 and n2:
+            prev_n1.next = n2
+            prev_n2.next = n1
+            n1.next, n2.next = n2.next,n1.next
+        
+        return dummy.next
+
