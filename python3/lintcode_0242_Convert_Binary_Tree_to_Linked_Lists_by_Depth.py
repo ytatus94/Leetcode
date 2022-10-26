3a2b1d1c40e3ebd7679491b9e7fd42cd9c79616e
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-10-26 14:43:01 -0700

    Update lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py

--- python3/lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py
+++ python3/lintcode_0242_Convert_Binary_Tree_to_Linked_Lists_by_Depth.py
@@ -1,3 +1,9 @@
+# 這題也是層及遍歷，用 BFS
+# 用 Binary Tree Level Order Traversal 模板先找出每一層的元素
+# 每一層的元素會組成一個 linked list 所以有 n 層就會有 n 個 linked list
+# 找出每一層的元素後，用這些元素組成一個 linked list 再塞到 results 裡面
+
+# 方法1: 用 Binary Tree Level Order Traversal 的模板
 """
 Definition of TreeNode:
 class TreeNode:
@@ -30,7 +36,8 @@ class Solution:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            print(curr_level)
+            # 這上面的部分就和 Binary Tree Lever Order Traversal 一樣
+            # 找出該層所有元素後，把這些元素組成一個 linked list 再放到 results 裡面
             head = self.list_to_linkedlist(curr_level)
             results.append(head)
 
@@ -45,7 +52,7 @@ class Solution:
             curr = curr.next
         return dummy.next
 
-# 方法2: 比較快
+# 方法2: 比較快，因為一邊找出每一層的元素，一邊建立 linked list
 """
 Definition of TreeNode:
 class TreeNode:
@@ -88,6 +95,7 @@ class Solution:
 
         return results
 
+# 方法3:
 """
 Definition of TreeNode:
 class TreeNode:
@@ -113,9 +121,9 @@ class Solution:
         while queue:
             # 現在這層的節點
             curr_layer = [node.val for node in queue]
-            print(curr_layer)
             head = self.convert_list_to_linked_list(curr_layer)
             results.append(head)
+            
             # 把下一層的結點加入到 queue 中
             next_layer = []
             for node in queue:
