11288edf4e4c4e1e227d390c2e0459711ef137d1
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-10-25 23:56:37 -0700

    Update lintcode_0096_Partition_List.py

--- python3/lintcode_0096_Partition_List.py
+++ python3/lintcode_0096_Partition_List.py
@@ -26,12 +26,13 @@ class Solution:
         while head:
             if head.val < x:
                 curr1.next = head
-                curr1 = head
+                curr1 = head # curr1 往下移動一格
             else:
                 curr2.next = head
-                curr2 = head
+                curr2 = head # curr2 往下移動一格
             head = head.next
 
-        curr2.next = None
-        curr1.next = dummy2.next
-        return dummy1.next
+        curr2.next = None # 要先把 curr2 的最後面接上 None
+        curr1.next = dummy2.next # 然後再把 curr1 的後面接上第二個 linked list 的頭
+        return dummy1.next # 要傳回第一個 linked list 的頭
+
