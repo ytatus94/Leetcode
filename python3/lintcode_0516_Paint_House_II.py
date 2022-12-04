be84c8ba443b7f8cc36c6d826b103b362e8d127b
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-04 00:10:21 -0800

    Create lintcode_0516_Paint_House_II.py

---
+++ python3/lintcode_0516_Paint_House_II.py
@@ -0,0 +1,40 @@
+# 方法 1: 會超時
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param costs: n x k cost matrix
+    @return: an integer, the minimum cost to paint all houses
+    """
+    def min_cost_i_i(self, costs: List[List[int]]) -> int:
+        # write your code here
+        n = len(costs) # number of houses
+        if n == 0:
+            return 0
+        k = len(costs[0]) # number of colors
+        if k == 0:
+            return 0
+
+        # Create a num_of_houses x num_of_colors array
+        # f[i][j] = minimum cost of painting the first i houses 
+        # and the color of house i-1 is j
+        f = [[float('inf') for j in range(k)] for i in range(n + 1)]
+
+        # initialize
+        for j in range(k):
+            f[0][j] = 0 # paint the firs 0 house cost 0
+
+        for i in range(1, n + 1):
+            if k > 1:
+                for j in range(k): # color of current house i
+                    for c in range(k): # color of previous house i-1
+                        if c != j:
+                            f[i][j] = min(f[i][j], f[i - 1][c] + costs[i - 1][j])
+            else:
+                f[i][j] = costs[i - 1][j]
+
+        print(f)
+        return min(f[n])
+                
