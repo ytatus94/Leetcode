a834b55d8affd86a1e26f079e19442cceca84fb7
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-03 20:21:27 -0800

    Update 338_Counting_Bits.py

--- python3/338_Counting_Bits.py
+++ python3/338_Counting_Bits.py
@@ -1,3 +1,5 @@
+# 位操作型 DP, 計數型 DP
+# TC = O(N), SC = O(N)
 class Solution:
     def countBits(self, n: int) -> List[int]:
         # f[i] 表示數字 i 的二進位有多少個 1
@@ -11,3 +13,8 @@ class Solution:
             f[i] = (i % 2) + f[i >> 1]
 
         return f
+
+# i % 2 和 i & 1 會得到相同的結果，而且 i & 1 會比較快
+# i 的二進制最後一位是 1 時 i & 1 會是 1
+# i 的二進制最後一位是 0 時 i & 1 會是 0
+   
