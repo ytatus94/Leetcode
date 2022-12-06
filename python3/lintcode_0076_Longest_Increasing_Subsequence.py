95e9246278d479f4a22c05dc20bf9f02e41ac24b
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-05 17:47:35 -0800

    Update lintcode_0076_Longest_Increasing_Subsequence.py

--- python3/lintcode_0076_Longest_Increasing_Subsequence.py
+++ python3/lintcode_0076_Longest_Increasing_Subsequence.py
@@ -1,4 +1,4 @@
-# 最長序列型 DP, 計數型
+# 最長序列型 DP (座標型), 計數型 DP
 # 轉移方程:
 #   f[i] = max(1, f[j]+1|(i>j&&a[i]>a[j]) )
 # 初始條件: 沒有初始條件
@@ -22,13 +22,16 @@ class Solution:
             return 0
 
         # Create an array
-        f = [0 for i in range(n)]
+        f = [0 for i in range(n)] # 因為是座標型，只要開 n 就可以了
         # f[i] means the LIS ending with a[i]
 
         # No initial condition
 
         for i in range(n):
+            # case 1: 只有自己
             f[i] = 1 # 如果前面的數都比 a[i] 大，那子序列就是 a[i] 自己
+            
+            # case 2: a[i] 是序列的最後一個數
             for j in range(i): # 枚舉 a[i] 前面的所有的數目
                 if nums[i] > nums[j]:
                     f[i] = max(f[i], f[j] + 1)
