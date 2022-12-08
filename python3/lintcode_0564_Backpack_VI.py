40ccb00aef700b5062811049546fdc626c6acbf7
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-08 00:10:44 -0800

    Update lintcode_0564_Backpack_VI.py

--- python3/lintcode_0564_Backpack_VI.py
+++ python3/lintcode_0564_Backpack_VI.py
@@ -1,4 +1,13 @@
-    if nums is None:
+# 背包型 DP, 計數型 DP
+# 轉移方程
+#   f[i] = f[i -a[0]] + f[i-a[1]] +...+ f[i-a[n-1]]
+#   f[i] = 有多少種組合能拼出 i
+#   要注意 i > a[j] 的時候才能加入到公式裡面
+# 初始條件
+# f[0] = 1 有一種組合能拼出 0 (就是什麼都不選的時候)
+# TC = O(N * target) = 計算步數, SC = O(target)
+
+if nums is None:
       return 0
     n = len(nums)
     if n == 0:
