d2eccaaebcb2acc0cbd806d170a71ec220a3de36
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-08 00:02:15 -0800

    Update lintcode_0092_Backpack.py

--- python3/lintcode_0092_Backpack.py
+++ python3/lintcode_0092_Backpack.py
@@ -52,3 +52,64 @@ class Solution:
                 break
 
         return results
+
+    
+# 如果要把背包內的物品印出來
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param a: Given n items with size A[i]
+    @return: The maximum size
+    """
+    def back_pack(self, m: int, a: List[int]) -> int:
+        # write your code here
+        if a is None:
+            return 0
+        n = len(a)
+        if n == 0:
+            return 0
+
+        # 建立一個數組紀錄前 i 個物品可以拼出的所有種可能重量 w
+        # f[i][w=0~m] = 前 i 個物品能否拼出重量 w, w 從 0 ~ m
+        f = [[False for j in range(m + 1)] for i in range(n + 1)]
+
+        # 把放到背包的物品印出來
+        pi = [[0 for j in range(m + 1)] for i in range(n + 1)]
+        # pi[i][j] = 0: 不用第 i 個
+        # pi[i][j] = 1: 用第 i 個
+
+        # 初始化
+        f[0][0] = True # 前 0 個物品可以拼出重量 0
+        for i in range(1, m + 1):
+            f[0][i] = False # 前 0 個物品不可能拼出大於 0 的重量
+        
+        # 要由前 i 個物品拼出重量 j 有兩種可能
+        # 1. 前 i-1 個物品已經能拼出重量 j
+        # 2. 前 i-1 個物品只能拼出 j - A[i-1], 再加上第 i 個物品的重量 A[i-1] 就能拼出重量 j
+        for i in range(1, n + 1):
+            for j in range(0, m + 1):
+                f[i][j] = f[i - 1][j] # 前 i-1 個物品已經能拼出重量 j
+                # pi[i][j] = 0
+                if j >= a[i - 1]:
+                    if f[i - 1][j - a[i - 1]]: # 這時候會用到第 i 個
+                        pi[i][j] = 1
+                    f[i][j] |= f[i - 1][j - a[i - 1]]
+
+        results = 0
+        for i in range(m, -1, -1):
+            if f[n][i] == True:
+                results = i
+                break
+
+        print('Put the following items in the backpack')
+        i = results
+        for j in range(n, 0, -1):
+            if pi[j][i] == 1:
+                print(a[j - 1])
+                i -= a[j - 1]
+
+        return results
