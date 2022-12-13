84f237851f47d4606ea658f6cd35def0b38e2732
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-12 17:37:01 -0800

    Update lintcode_0440_Backpack_III.py

--- python3/lintcode_0440_Backpack_III.py
+++ python3/lintcode_0440_Backpack_III.py
@@ -47,3 +47,42 @@ class Solution:
                     f[i][w] = max(f[i][w], f[i][w - a[i - 1]] + v[i - 1])
 
         return max(f[n])
+
+# 空間優化
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param a: an integer array
+    @param v: an integer array
+    @param m: An integer
+    @return: an array
+    """
+    def back_pack_i_i_i(self, a: List[int], v: List[int], m: int) -> int:
+        # write your code here
+        if a is None:
+            return 0
+        n = len(a)
+        if n == 0:
+            return 0
+
+        # 當物品能有無限多個的時候，就只能用種類計算
+        # f[i][w] = 前 i 種物品組成重量 w 時的最大價值
+        # = max_{0<=k}(f[i-1][w-k*A[i-1]]+k*V[i-1])
+        # 前 i-1 種物品拼出 w-k*A[i-1] 的最大價值，加上第 i 種物品有 k 個的價值
+        # Backpack II 是 k = 0, 1 的特例
+        # f[i][w] = max(f[i-1][w], f[i-1][w-A[i-1]]+V[i-1], f[i-1][w-2*A[i-1]]+2*V[i-1],...)
+        # = max(f[i-1][w], f[i][w-A[i-1]]+V[i-1])
+
+        f = [-1 for w in range(m + 1)]
+        # initial condition
+        f[0] = 0
+
+        for i in range(1, n + 1):
+            for w in range(0, m + 1):
+                if w - a[i - 1] >= 0 and f[w - a[i - 1]] != -1: # 注意這邊看的是 f[i], backpack 2 看的是 i-1
+                    f[w] = max(f[w], f[w - a[i - 1]] + v[i - 1])
+
+        return max(f)
