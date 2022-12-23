e7249fc328735811223c0fa0ff13fcc16b5ddc10
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-23 11:57:34 -0800

    Update 176_Second_Highest_Salary.sql

--- SQL/176_Second_Highest_Salary.sql
+++ SQL/176_Second_Highest_Salary.sql
@@ -1,9 +1,11 @@
 # 有可能沒有第二高的薪水，這時候要傳回 null
-SELECT (SELECT DISTINCT Salary
-        FROM Employee
-        ORDER BY 1 DESC
-        LIMIT 1
-        OFFSET 1) AS SecondHighestSalary;
+SELECT (
+    SELECT DISTINCT Salary
+    FROM Employee
+    ORDER BY 1 DESC
+    LIMIT 1
+    OFFSET 1
+) AS SecondHighestSalary;
 
 # 方法二:
 SELECT MAX(Salary) AS SecondHighestSalary
