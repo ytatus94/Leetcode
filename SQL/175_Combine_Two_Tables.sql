09c1a43b2b3e3d259837682700e2a7f1c258e857
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-23 11:41:27 -0800

    Update 175_Combine_Two_Tables.sql

--- SQL/175_Combine_Two_Tables.sql
+++ SQL/175_Combine_Two_Tables.sql
@@ -1,4 +1,7 @@
-SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+SELECT Person.FirstName,
+       Person.LastName,
+       Address.City,
+       Address.State
 FROM Person
 LEFT JOIN Address
 ON Person.PersonID = Address.PersonId;
