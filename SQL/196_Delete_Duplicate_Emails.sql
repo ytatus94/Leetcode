684330d3cecc7ccd65837d7ce2e0df40d1259be9
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-26 15:45:36 -0800

    Update 196_Delete_Duplicate_Emails.sql

--- SQL/196_Delete_Duplicate_Emails.sql
+++ SQL/196_Delete_Duplicate_Emails.sql
@@ -2,3 +2,17 @@ DELETE p1
 FROM Person AS p1 
 JOIN Person AS p2
 ON p1.Email = p2.Email AND p1.Id > p2.Id;
+
+-- 先選出每個 email 中最小的 id
+-- 再把不是這些 id 的 rows 刪除
+-- 需要多一個 SELECT * FROM() AS p 是因為不能在 Person 中刪除依照 Person Query 出來的結果
+-- 所以先產生一個 p 就沒事
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT *
+    FROM (
+        SELECT MIN(id)
+        FROM Person
+        GROUP BY email
+    ) AS p
+)
