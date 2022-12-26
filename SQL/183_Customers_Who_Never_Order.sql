83bd585cd7a9c5d4b8d0156401eaf623528d2a0d
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-26 11:34:46 -0800

    Update 183_Customers_Who_Never_Order.sql

--- SQL/183_Customers_Who_Never_Order.sql
+++ SQL/183_Customers_Who_Never_Order.sql
@@ -1,4 +1,13 @@
-SELECT Customers.Name AS Customers FROM Customers
+SELECT Customers.Name AS Customers
+FROM Customers
 LEFT JOIN Orders
 ON Customers.Id = Orders.CustomerID
 WHERE Orders.CustomerID IS NULL;
+
+
+-- 另一個寫法
+SELECT name AS Customers
+FROM Customers AS c
+LEFT JOIN Orders AS o
+ON c.id = o.customerId
+WHERE o.customerId IS NULL
