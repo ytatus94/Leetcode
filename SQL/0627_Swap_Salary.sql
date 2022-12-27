d6955476d0f86ee6630345d816b7b978d3b5013c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-27 15:53:19 -0800

    Update and rename 627_Swap_Salary.sql to 0627_Swap_Salary.sql

---
+++ SQL/0627_Swap_Salary.sql
@@ -0,0 +1,16 @@
+-- 需要用 if condition 來交換 f 和 m
+-- 有兩種 if conditions:
+-- IF(condition, value_if_true, value_if_false)
+-- CASE WHEN condition1 THEN result1 ... WHEN conditionN THEN resultN ELSE result END;
+
+-- 方法1.
+UPDATE salary
+SET sex = 
+  CASE
+    WHEN sex = 'm' THEN 'f'
+    ELSE 'm'
+  END;
+
+-- 方法2.
+UPDATE Salary
+SET sex = IF(sex = 'm', 'f', 'm')
