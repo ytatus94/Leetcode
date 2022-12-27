98e7cc185bb4da3f85922575afae10c1721dd50a
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-26 16:02:13 -0800

    Update 197_Rising_Temperature.sql

--- SQL/197_Rising_Temperature.sql
+++ SQL/197_Rising_Temperature.sql
@@ -3,3 +3,9 @@ FROM Weather AS w1
 JOIN Weather AS w2
 ON DATEDIFF(w1.RecordDate, w2.RecordDate) = 1
 WHERE w1.Temperature > w2.Temperature
+
+-- 方法 2
+SELECT w2.id AS id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w2.recordDate, w1.recordDate) = 1
+AND w2.temperature > w1.temperature 
