65a46dd3cb11f858da2167ff298d253d740e993c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-26 21:48:51 -0800

    Update 1795_Rearrange_Products_Table.sql

--- SQL/1795_Rearrange_Products_Table.sql
+++ SQL/1795_Rearrange_Products_Table.sql
@@ -1,3 +1,4 @@
+-- 方法 1
 SELECT product_id,
        "store1" AS store,
        CASE
@@ -19,5 +20,26 @@ SELECT product_id,
        CASE
            WHEN store3 THEN store3
        END AS price
+FROM Products
+WHERE store3 IS NOT NULL
+
+
+-- 方法 2
+-- 其實不需要 CASE 述句
+SELECT product_id,
+       "store1" AS store,
+       store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+UNION
+SELECT product_id,
+       "store2" AS store,
+       store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+UNION
+SELECT product_id,
+       "store3" AS store,
+       store3 AS price
 FROM Products
 WHERE store3 IS NOT NULL
