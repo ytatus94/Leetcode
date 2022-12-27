25fe3bd883934dbe7f4fde51359a55e06a59080c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-27 00:22:44 -0800

    Create 1393_Capital_Gain_Loss.sql

---
+++ SQL/1393_Capital_Gain_Loss.sql
@@ -0,0 +1,17 @@
+SELECT s1.stock_name,
+       s1.price * -1 + s2.price AS capital_gain_loss
+FROM (
+    SELECT stock_name,
+           SUM(price) AS price
+    FROM Stocks
+    WHERE operation = 'Buy'
+    GROUP BY stock_name
+) AS s1
+JOIN (
+    SELECT stock_name,
+           SUM(price) AS price
+    FROM Stocks
+    WHERE operation = 'Sell'
+    GROUP BY stock_name
+) AS s2
+ON s1.stock_name = s2.stock_name
