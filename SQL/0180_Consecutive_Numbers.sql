062557e4dc0d049de0df769fca73c45c59c80be4
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 10:04:00 -0800

    Update 0180_Consecutive_Numbers.sql

--- SQL/0180_Consecutive_Numbers.sql
+++ SQL/0180_Consecutive_Numbers.sql
@@ -1,6 +1,8 @@
 -- 考會不會做 self-join
 -- 如果用 FROM 來 join 表格，那連接的條件就用 WHERE，而且這種方式的 join 是 inner join
+
 -- 把 Logs 表格做 self-join
+-- 注意 WHERE 部分的寫法，這樣可以確定三個 id 是連續的
 SELECT DISTINCT l1.num AS ConsecutiveNums
 FROM Logs AS l1, Logs AS l2, Logs AS l3
 WHERE l1.id + 1 = l2.id AND l2.id + 1 = l3.id
