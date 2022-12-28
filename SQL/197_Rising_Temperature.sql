f3275771c855ebef777743fc1df950c2048447ab
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 11:51:33 -0800

    Update 197_Rising_Temperature.sql

--- SQL/197_Rising_Temperature.sql
+++ SQL/197_Rising_Temperature.sql
@@ -1,11 +1,13 @@
+-- 考會不會使用 DATEDIFF() 函數
+-- 方法 1
 SELECT w1.Id
 FROM Weather AS w1
 JOIN Weather AS w2
 ON DATEDIFF(w1.RecordDate, w2.RecordDate) = 1
-WHERE w1.Temperature > w2.Temperature
+WHERE w1.Temperature > w2.Temperature;
 
 -- 方法 2
 SELECT w2.id AS id
 FROM Weather w1, Weather w2
 WHERE DATEDIFF(w2.recordDate, w1.recordDate) = 1
-AND w2.temperature > w1.temperature 
+AND w2.temperature > w1.temperature;
