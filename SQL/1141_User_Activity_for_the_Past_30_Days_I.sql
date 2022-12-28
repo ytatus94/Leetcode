d2f66222603b6c1415f0c92c6abea5432dc87cf3
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 15:23:51 -0800

    Update 1141_User_Activity_for_the_Past_30_Days_I.sql

--- SQL/1141_User_Activity_for_the_Past_30_Days_I.sql
+++ SQL/1141_User_Activity_for_the_Past_30_Days_I.sql
@@ -1,15 +1,20 @@
+-- 就是要知道怎麼選定時間範圍
+-- 2019-07-27 是第 30 天，那往前推 2019-06-28 是第一天
+-- INTERVAL 29 DAY 是因為要包含頭尾，所以只有 29 天的差別
+
 -- 方法1
 SELECT activity_date AS day,
        COUNT(DISTINCT user_id) AS active_users
 FROM Activity
 WHERE activity_date BETWEEN ("2019-07-27" - INTERVAL 29 DAY) AND "2019-07-27"
 GROUP BY 1
-ORDER BY 1
+ORDER BY 1;
 
 -- 方法2
-SELECT activity_date AS day,
-       COUNT(DISTINCT user_id) AS active_users
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
 FROM Activity
 WHERE activity_date >= '2019-06-28' AND activity_date <= '2019-07-27'
 GROUP BY 1
-ORDER BY 1
+ORDER BY 1;
