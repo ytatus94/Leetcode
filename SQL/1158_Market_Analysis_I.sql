f7b549908eea8bbf0a5948912ce0dad2daf2f653
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 15:47:56 -0800

    Update 1158_Market_Analysis_I.sql

--- SQL/1158_Market_Analysis_I.sql
+++ SQL/1158_Market_Analysis_I.sql
@@ -1,3 +1,6 @@
+-- 要會用 IFNULL(expression, alt_value) 判斷是不是 NULL 如果是 NULL 就設成 alt_value
+-- 在 sub query 中計算 2019 年每個 buyer 有多少交易，沒有交易的會顯示 NULL
+
 SELECT
     u.user_id AS buyer_id,
     u.join_date AS join_date,
@@ -11,4 +14,5 @@ LEFT JOIN (
     WHERE order_date >= '2019-01-01' AND order_date <= '2019-12-31'
     GROUP BY 1
 ) AS o
-ON u.user_id = o.buyer_id
+ON u.user_id = o.buyer_id;
+
