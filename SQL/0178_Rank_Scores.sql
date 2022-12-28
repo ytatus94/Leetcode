d29c1e450a4cbbfbbd8d4bec371cef1a521ac5cb
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 00:31:14 -0800

    Update 0178_Rank_Scores.sql

--- SQL/0178_Rank_Scores.sql
+++ SQL/0178_Rank_Scores.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
+-- 考會不會用 window function
 -- 注意 alias 中的 rank 要加上雙引號
-SELECT score,
-       DENSE_RANK() OVER (ORDER BY score DESC) AS "rank"
+SELECT
+    score,
+    DENSE_RANK() OVER (ORDER BY score DESC) AS "rank"
 FROM Scores
