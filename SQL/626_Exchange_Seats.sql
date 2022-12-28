e839b52a8c0435e14d7e3aed44b90cf32ae51ea3
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 12:42:41 -0800

    Update and rename 626_Exchange_Seats.sql to 0626_Exchange_Seats.sql

--- SQL/626_Exchange_Seats.sql
+++
@@ -1,9 +0,0 @@
-SELECT (CASE
-            WHEN MOD(id, 2) = 1 AND id = counts THEN id
-            WHEN MOD(id, 2) = 1 AND id != counts THEN id+1
-            ELSE id-1
-        END
-       ) AS id, student
-FROM seat, (SELECT COUNT(*) AS counts
-            FROM seat) AS seat_counts
-ORDER BY id ASC
