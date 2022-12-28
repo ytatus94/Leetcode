10a859dd7a80cd55ad00482e74866260b99f3e43
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 10:03:01 -0800

    Update 0178_Rank_Scores.sql

--- SQL/0178_Rank_Scores.sql
+++ SQL/0178_Rank_Scores.sql
@@ -1,6 +1,6 @@
 -- 考會不會用 window function
--- 注意 alias 中的 rank 要加上雙引號
+-- 注意 alias 中的 rank 要加上引號，不然會報錯，可能因為是要和 RANK() 函數區分開來的緣故
 SELECT
     score,
-    DENSE_RANK() OVER (ORDER BY score DESC) AS "rank"
+    DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
 FROM Scores
