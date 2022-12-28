8b40e59270bfac70cae77ee2fe4f4f9cf008ad81
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 00:07:13 -0800

    Create 262_Trips_and_Users.sql

---
+++ SQL/262_Trips_and_Users.sql
@@ -0,0 +1,8 @@
+SELECT
+    request_at AS Day,
+    ROUND(SUM(CASE WHEN status LIKE 'cancelled%' THEN 1 ELSE 0 END) / COUNT(request_at), 2) AS 'Cancellation Rate'
+FROM Trips
+WHERE client_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
+AND driver_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
+AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at
