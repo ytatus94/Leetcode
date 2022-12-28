0ae6bd7ae8865b1f72ada66353dfd4be162ae654
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 10:05:15 -0800

    Update 0180_Consecutive_Numbers.sql

--- SQL/0180_Consecutive_Numbers.sql
+++ SQL/0180_Consecutive_Numbers.sql
@@ -6,4 +6,5 @@
 SELECT DISTINCT l1.num AS ConsecutiveNums
 FROM Logs AS l1, Logs AS l2, Logs AS l3
 WHERE l1.id + 1 = l2.id AND l2.id + 1 = l3.id
-AND l1.num = l2.num AND l2.num = l3.num
+AND l1.num = l2.num AND l2.num = l3.num;
+
