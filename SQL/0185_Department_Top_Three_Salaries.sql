b9a66cb7854b8e1b8609a9b219509d2564d76be5
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 10:27:11 -0800

    Update 0185_Department_Top_Three_Salaries.sql

--- SQL/0185_Department_Top_Three_Salaries.sql
+++ SQL/0185_Department_Top_Three_Salaries.sql
@@ -1,4 +1,5 @@
--- 先用 DENSE_RANK() 對每個部門的薪水做降冪排序
+-- 先用 DENSE_RANK() 對每個部門的薪水做降冪排序，注意 alias 的 rank 要用引號括起來
+-- window function 不能用在 WHERE, GROUP BY, HAVING 裡面
 -- 在和 Department 表格做 JOIN 選出排名前三名的薪水
 SELECT d.name AS Department,
        e.name AS Employee,
