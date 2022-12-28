97f7308b9e4f145c72a842111d24b2b53f39a90c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-27 16:37:38 -0800

    Update 607_Sales_Person.sql

--- SQL/607_Sales_Person.sql
+++ SQL/607_Sales_Person.sql
@@ -1,7 +1,13 @@
+-- 先找出有來自 RED 公司訂單的 sales_id
+-- 然後排除這些 sales_id 就是答案
 SELECT name
 FROM salesperson
-WHERE sales_id NOT IN (SELECT o.sales_id
-                       FROM orders AS o
-                       JOIN company AS c
-                       ON o.com_id = c.com_id
-                       WHERE o.name = 'RED');
+WHERE sales_id NOT IN (
+    SELECT o.sales_id
+    FROM orders AS o
+    JOIN company AS c
+    ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
+
+
