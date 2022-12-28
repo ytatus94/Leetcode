d12e4c191023e3d3680a52d2cd7ce80fe850b281
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 11:00:38 -0800

    Update 0185_Department_Top_Three_Salaries.sql

--- SQL/0185_Department_Top_Three_Salaries.sql
+++ SQL/0185_Department_Top_Three_Salaries.sql
@@ -14,3 +14,33 @@ FROM (
 JOIN Department d
 ON e.departmentId = d.id
 WHERE e.rank <= 3
+
+-- 方法2 是 leetcode 提供的答案
+-- 合併 Employee 和 Department 後得到 (先不做任何篩選)
++----+-------+--------+--------------+----------------+
+| id | name  | salary | departmentId | departmentName |
++----+-------+--------+--------------+----------------+
+| 1  | Joe   | 85000  | 1            | IT             |
+| 2  | Henry | 80000  | 2            | Sales          |
+| 3  | Sam   | 60000  | 2            | Sales          |
+| 4  | Max   | 90000  | 1            | IT             |
+| 5  | Janet | 69000  | 1            | IT             |
+| 6  | Randy | 85000  | 1            | IT             |
+| 7  | Will  | 70000  | 1            | IT             |
++----+-------+--------+--------------+----------------+
+-- 要找每個部門的前三高的薪水，就一 row 一 row 的看，計算在同一部門中有多少薪水比當前 row 的薪水高
+-- 這就是 WHERE 中 sub query 在做的事情
+-- 部門做高薪 --> 0 row 比當前高，部門第二高薪 --> 只有 1 row 比當前高，部門第三高薪 --> 只有 2 row 比當前高
+-- 所以 WHERE 就用 3 > 多少薪水比當前高
+SELECT
+    d.Name AS 'Department',
+    e1.Name AS 'Employee',
+    e1.Salary
+FROM Employee e1
+JOIN Department d 
+ON e1.DepartmentId = d.Id
+WHERE 3 > ( -- 這裡的 3 是數字 3 不是 SELECT 中的第三個欄位
+    SELECT COUNT(DISTINCT e2.Salary)
+    FROM Employee e2
+    WHERE e2.Salary > e1.Salary AND e1.DepartmentId = e2.DepartmentId
+);
