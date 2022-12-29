6e47a1b3d7d2c013b1c9f5aa80a98322e349e6c0
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:19:57 -0800

    Update 1587_Bank_Account_Summary_II.sql

--- SQL/1587_Bank_Account_Summary_II.sql
+++ SQL/1587_Bank_Account_Summary_II.sql
@@ -1,7 +1,8 @@
+-- 要會用 HAVING 篩選 aggregation function
 SELECT u.name AS name,
        SUM(t.amount) AS balance
 FROM Users AS u
 LEFT JOIN Transactions AS t
 ON u.account = t.account
 GROUP BY 1
-HAVING SUM(t.amount) > 10000
+HAVING SUM(t.amount) > 10000;
