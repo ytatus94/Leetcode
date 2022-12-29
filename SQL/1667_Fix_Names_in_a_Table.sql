95a3a3f3005bd346a8ea4e2dba9ce89eaea4cc42
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:24:47 -0800

    Update 1667_Fix_Names_in_a_Table.sql

--- SQL/1667_Fix_Names_in_a_Table.sql
+++ SQL/1667_Fix_Names_in_a_Table.sql
@@ -1,3 +1,11 @@
+-- 要會用函數
+-- CONCAT(string1, string2, ...., string_n): 結合字串
+-- UPPER(text): 全都變大寫
+-- LOWER(text): 全都變小寫
+-- LEFT(string, number_of_chars): 從 string 左邊選曲 number_of_chars 個字元
+-- SUBSTR(string, start, length) 或是 SUBSTR(string FROM start FOR length): 從 string 選取從 start 開始長度為 length 的子字串
+-- SUBSTRING(string, start, length) 和 SUBSTR(string, start, length) 等價
+
 SELECT user_id,
        CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTR(name, 2))) AS name
 FROM Users
