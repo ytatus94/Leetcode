ae100042ffe3168ef590b8d1a8c479096c3567da
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:28:34 -0800

    Update 1729_Find_Followers_Count.sql

--- SQL/1729_Find_Followers_Count.sql
+++ SQL/1729_Find_Followers_Count.sql
@@ -1,5 +1,8 @@
-SELECT user_id,
-       COUNT(follower_id) AS followers_count
+-- 要會用 COUNT(), GROUP BY, ORDER BY
+-- 有 GROUP BY 就一定要有欄位用到 aggregation function
+SELECT
+    user_id,
+    COUNT(follower_id) AS followers_count
 FROM Followers
 GROUP BY 1
 ORDER BY 1
