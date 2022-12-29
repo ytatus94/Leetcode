23edd85021a9992bae43513587bde069d7239192
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:13:25 -0800

    Update 1407_Top_Travellers.sql

--- SQL/1407_Top_Travellers.sql
+++ SQL/1407_Top_Travellers.sql
@@ -1,7 +1,9 @@
+-- IFNULL(expression, alt_value)
+-- ORDER BY 裡面可以用 aggregation function
 SELECT u.name AS name,
        IFNULL(SUM(r.distance), 0) AS travelled_distance
 FROM Users AS u
 LEFT JOIN Rides AS r
 ON u.id = r.user_id
 GROUP BY u.id
-ORDER BY SUM(r.distance) DESC, u.name
+ORDER BY SUM(r.distance) DESC, u.name;
