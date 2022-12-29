335c26774fcf090a61ee90927801ea7ce884a1af
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:15:23 -0800

    Update 1527_Patients_With_a_Condition.sql

--- SQL/1527_Patients_With_a_Condition.sql
+++ SQL/1527_Patients_With_a_Condition.sql
@@ -1,5 +1,6 @@
+-- 要知道怎麼用通配符 %
 SELECT patient_id,
        patient_name,
        conditions
 FROM Patients
-WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
