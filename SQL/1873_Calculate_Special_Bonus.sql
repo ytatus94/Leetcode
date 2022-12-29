e6c5f1cb45d595670d3cb1901de51f1bfa9d2c03
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:47:08 -0800

    Update 1873_Calculate_Special_Bonus.sql

--- SQL/1873_Calculate_Special_Bonus.sql
+++ SQL/1873_Calculate_Special_Bonus.sql
@@ -1,7 +1,10 @@
-SELECT employee_id,
-       CASE
-           WHEN employee_id % 2 = 1 AND name NOT LIKE 'M%' THEN salary 
-           ELSE 0 
-       END AS bonus
+-- CASE WHEN ... THEN ... ELSE ... END
+-- 要會算 mod 和要會通配符
+SELECT
+    employee_id,
+    CASE
+        WHEN employee_id % 2 = 1 AND name NOT LIKE 'M%' THEN salary 
+        ELSE 0 
+    END AS bonus
 FROM Employees
-ORDER BY 1
+ORDER BY 1;
