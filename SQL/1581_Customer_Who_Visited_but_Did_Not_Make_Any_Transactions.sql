bd30e2b68eb5b4497accfd2b470fa8795bd5387c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:18:30 -0800

    Update 1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql

--- SQL/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
+++ SQL/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -1,7 +1,9 @@
+-- 要知道怎麼篩選出沒有交易的客戶
+-- 合併表格，選出沒有交易的客戶，最後計算數目
 SELECT v.customer_id,
        COUNT(v.customer_id) AS count_no_trans
 FROM Visits v
 LEFT JOIN Transactions t
 ON v.visit_id = t.visit_id
 WHERE t.transaction_id IS NULL
-GROUP BY 1
+GROUP BY 1;
