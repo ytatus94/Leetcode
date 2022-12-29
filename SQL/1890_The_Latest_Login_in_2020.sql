635beeafb62d717f0eba183f5aa7c5f1f293c51c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:48:03 -0800

    Update 1890_The_Latest_Login_in_2020.sql

--- SQL/1890_The_Latest_Login_in_2020.sql
+++ SQL/1890_The_Latest_Login_in_2020.sql
@@ -1,6 +1,7 @@
-# Write your MySQL query statement below
+-- EXTRACT(part FROM date)
 SELECT user_id,
        MAX(time_stamp) AS last_stamp
 FROM Logins
 WHERE EXTRACT(YEAR FROM time_stamp) = 2020
-GROUP BY user_id
+GROUP BY user_id;
+
