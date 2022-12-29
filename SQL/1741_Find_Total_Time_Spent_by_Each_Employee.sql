37c58d208f19b8e982d21bc578112bbc1582a710
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:29:32 -0800

    Update 1741_Find_Total_Time_Spent_by_Each_Employee.sql

--- SQL/1741_Find_Total_Time_Spent_by_Each_Employee.sql
+++ SQL/1741_Find_Total_Time_Spent_by_Each_Employee.sql
@@ -1,5 +1,8 @@
-SELECT event_day AS day,
-       emp_id,
-       SUM(out_time - in_time) AS total_time
+-- 要會用 SUM(), GROUP BY
+-- 有 GROUP BY 就要有 aggregation function 欄位
+SELECT
+    event_day AS day,
+    emp_id,
+    SUM(out_time - in_time) AS total_time
 FROM Employees
 GROUP BY 1, 2
