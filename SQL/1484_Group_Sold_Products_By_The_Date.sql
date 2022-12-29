a0ee1db73675eac70038288c606e47ca5674ae66
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2022-12-28 21:14:37 -0800

    Update 1484_Group_Sold_Products_By_The_Date.sql

--- SQL/1484_Group_Sold_Products_By_The_Date.sql
+++ SQL/1484_Group_Sold_Products_By_The_Date.sql
@@ -1,4 +1,5 @@
 -- 這一題只要知道 GROUP_CONCAT() 就能解決
+-- GROUP_CONCAT(expr) 是 MySQL 才支援的函數
 SELECT sell_date,
        COUNT(DISTINCT product) AS num_sold,
        GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') as products      
