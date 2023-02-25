e196452cfa7a54c5dfd8721f4c4d7a4ef98db3a6
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-02-25 10:33:41 -0800

    Update and rename 064_Minimum_Path_Sum.py to 0064_Minimum_Path_Sum.py

--- python3/0064_Minimum_Path_Sum.py
+++ python3/0064_Minimum_Path_Sum.py
@@ -17,7 +17,25 @@ class Solution:
                     grid[row][col] += min(grid[row][col - 1], grid[row - 1][col])
                     
         return grid[rows - 1][cols - 1]
-        
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        # 開一個二維陣列紀錄到 (i, j) 的最小路徑和
+        dp = [[0] * n] * m
+        for i in range(0, m):
+            for j in range(0, n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                elif i == 0: # 只能從左邊一格走過來
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+                elif j == 0: # 只能從上面一格走過來
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j]
+        return dp[m - 1][n - 1]
+
 # lintcode 110
 class Solution:
     """
