8bb152155863a02c9af3e7b9d72e9768264202f5
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-03-23 14:12:49 -0700

    Update 509_Fibonacci_Number.py

--- python3/509_Fibonacci_Number.py
+++ python3/509_Fibonacci_Number.py
@@ -16,3 +16,18 @@ class Solution:
                 temp[0] = temp[1]
                 temp[1] = sum
         return sum
+
+# 方法 2
+class Solution:
+    """
+    @param n: an integer
+    @return: an ineger f(n)
+    """
+    def fibonacci(self, n: int) -> int:
+        # write your code here
+        f = [None for i in range(n + 1)]
+        f[0] = 0
+        f[1] = 1
+        for i in range(2, n + 1):
+            f[i] = f[i - 1] + f[i - 2]
+        return f[n - 1] # because n counts from 1 and i count from 0
