118bfddb59c40400846d36c0320dd1bb1b2b41f8
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-03-23 22:58:30 -0700

    Update lintcode_0037_Reverse_3-digit_Integer.py

--- python3/lintcode_0037_Reverse_3-digit_Integer.py
+++ python3/lintcode_0037_Reverse_3-digit_Integer.py
@@ -7,3 +7,15 @@ class Solution:
         # write your code here
         res = str(number)[2] + str(number)[1] + str(number)[0]
         return int(res)
+
+class Solution:
+    """
+    @param number: A 3-digit number.
+    @return: Reversed number.
+    """
+    def reverse_integer(self, number: int) -> int:
+        # write your code here
+        c = number % 10
+        b = ((number - c) // 10) % 10
+        a = ((number - (b * 10 + c)) // 100) % 10
+        return c * 100 + b * 10 + a
