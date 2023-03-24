f4f5801c7a2f0a546162de286aa8820a4fc2a20b
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-03-23 17:21:00 -0700

    Update 0912_Sort_an_Array.py

--- python3/0912_Sort_an_Array.py
+++ python3/0912_Sort_an_Array.py
@@ -25,3 +25,47 @@ class Solution:
         self.quick_sort(nums, start, right)
         self.quick_sort(nums, left, end)
     
+# 用 merge sort TC = O(NlogN) 不會超時
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) == 1:
+            return nums
+        temp = [0 for _ in range(len(nums))]
+        self.merge_sort(temp, nums, 0, len(nums) - 1)
+        return nums
+
+    def merge_sort(self, temp: List[int], nums: List[int], start: int, end: int) -> None:
+        if start >= end:
+            return
+
+        mid = (start + end) // 2
+        
+        self.merge_sort(temp, nums, start, mid)
+        self.merge_sort(temp, nums, mid+1, end)
+        self.merge(temp, nums, start, mid, end)
+
+    def merge(self, temp, nums, start, mid, end):
+        left = start
+        right = mid + 1
+        idx = start
+
+        while left <= mid and right <= end:
+            if nums[left] < nums[right]:
+                temp[idx] = nums[left]
+                left += 1
+            else:
+                temp[idx] = nums[right]
+                right += 1
+            idx += 1
+        while left <= mid:
+            temp[idx] = nums[left]
+            left += 1
+            idx += 1
+        while right <= end:
+            temp[idx] = nums[right]
+            right += 1
+            idx += 1
+
+        for i in range(start, end+1):
+            nums[i] = temp[i]
+
