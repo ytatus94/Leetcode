f31936895e24890483d1b872ea87acef4b133435
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-03-30 21:39:50 -0700

    Update 0152_Maximum_Product_Subarray.py

--- python3/0152_Maximum_Product_Subarray.py
+++ python3/0152_Maximum_Product_Subarray.py
@@ -7,6 +7,7 @@ class Solution:
         dp_max[0] = nums[0]
         dp_min[0] = nums[0]
 
+        # subarray 的最大乘積可能是元素 i 自己，也可能是 nums[i] 之前的最大乘積再乘上 nums[i]
         for i in range(1, len(nums)):
             dp_max[i] = max(nums[i], max(nums[i] * dp_max[i - 1], nums[i] * dp_min[i - 1]))
             dp_min[i] = min(nums[i], min(nums[i] * dp_max[i - 1], nums[i] * dp_min[i - 1]))
