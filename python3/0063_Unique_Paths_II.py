9609c7944e04fdbc1c09b72b43a715220b94c73a
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-03-30 17:20:05 -0700

    Update 0063_Unique_Paths_II.py

--- python3/0063_Unique_Paths_II.py
+++ python3/0063_Unique_Paths_II.py
@@ -1,4 +1,30 @@
 class Solution:
+    """
+    @param obstacleGrid: A list of lists of integers
+    @return: An integer
+    """
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        # write your code here
+        if obstacleGrid is None or len(obstacleGrid) == 0 or len(obstacleGrid[0]) == 0:
+            return -1
+            
+        rows = len(obstacleGrid)
+        cols = len(obstacleGrid[0])
+        
+        grid = [[0 for col in range(cols)] for row in range(rows)]
+        
+        for row in range(rows):
+            for col in range(cols):
+                if obstacleGrid[row][col] == 1:
+                    grid[row][col] = 0 # 把障礙點設成 0
+                elif row == 0 and col == 0:
+                    grid[row][col] = 1
+                else:
+                    grid[row][col] = grid[row - 1][col] + grid[row][col - 1]
+
+        return grid[rows - 1][cols - 1]
+
+class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
