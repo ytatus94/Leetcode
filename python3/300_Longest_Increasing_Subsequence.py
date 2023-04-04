6c3b3960b31a7352de2544fbcb7342e4960f3cc0
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-04-03 17:49:48 -0700

    Update 300_Longest_Increasing_Subsequence.py

--- python3/300_Longest_Increasing_Subsequence.py
+++ python3/300_Longest_Increasing_Subsequence.py
@@ -1,3 +1,4 @@
+# 方法1: 暴力法 O(N^2)
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         if nums is None or len(nums) == 0:
@@ -12,6 +13,49 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1)
             
         return max(dp)
+
+# 方法2: 用二分法 O(NlogN) 比較快
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if nums is None or len(nums) == 0:
+            return 0
+
+        # 建立用來保存 LIS 的 list
+        # 用 binary search, dp 只會有正確的長度，不一定會有正確的 LIS 結果
+        # 這是因為把許多可能的 LIS 都不斷地重複寫在同一個 dp 的記憶體上面 
+        dp = []
+
+        # 先把第一個數放入
+        dp.append(nums[0])
+
+        for i in range(1, len(nums)):
+            if nums[i] > dp[-1]:
+                dp.append(nums[i]) # 如果 nums[i] 比目前 LIS 中最後一個還大，就加入 LIS 中
+            else:
+                # 找出 dp[j - 1] < nums[i] < dp[j] 的部分，然後把 dp[j] 用 nums[i] 取代 
+                insert_index = self.binary_search(dp, nums[i])
+                dp[insert_index] = nums[i]
+        
+        return len(dp)
+    
+    def binary_search(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+
+        while (start + 1 < end):
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target: # target 在後半段，所以把 start 移到 mid
+                start = mid
+            elif nums[mid] > target: # target 在前半段，所以把 end 移到 mid
+                end = mid
+
+        if nums[start] < target:
+            return start + 1
+        elif nums[start] >= target:
+            return start
+        
 
 # lintcode 076
 class Solution:
