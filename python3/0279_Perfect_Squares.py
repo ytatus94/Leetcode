a2841f3cb832455e4dbae96d0db756fd380c6785
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-04-04 09:58:50 -0700

    Update 0279_Perfect_Squares.py

--- python3/0279_Perfect_Squares.py
+++ python3/0279_Perfect_Squares.py
@@ -1,3 +1,12 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float('inf') for i in range(n + 1)]
+        dp[0] = 0
+        for i in range(1, n + 1):
+            for j in range(1, int(i ** 0.5) + 1): # j 跑到 sqrt(i) 就好
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+        return dp[n]
+
 # O(N^2) 超時了
 class Solution:
     def numSquares(self, n: int) -> int:
