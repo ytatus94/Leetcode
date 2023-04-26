6cec1bb52a91ad27efa42e231ce28155c3eb31e5
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-04-26 11:08:49 -0700

    Update lintcode_0481_Binary_Tree_Leaf_Sum.py

--- lintcode/lintcode_0481_Binary_Tree_Leaf_Sum.py
+++ lintcode/lintcode_0481_Binary_Tree_Leaf_Sum.py
@@ -1,4 +1,15 @@
-def leafSum(self, root):
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+"""
+class Solution:
+    # @param {TreeNode} root the root of the binary tree
+    # @return {int} an integer
+    def leafSum(self, root):
+        # Write your code here
         result = []
         self.dfs(root, result)
         return sum(result)
