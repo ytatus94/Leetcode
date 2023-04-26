4c7babc385f5e244ff1a8be900ba829099af0529
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-04-26 16:12:17 -0700

    Update 0002_Add_Two_Numbers.py

--- python3/0002_Add_Two_Numbers.py
+++ python3/0002_Add_Two_Numbers.py
@@ -53,3 +53,31 @@ class Solution:
             curr = curr.next
 
         return dummy.next
+
+# 方法 2: 把上面的三個 while 和一個 if 合併起來
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+
+        temp = 0 # 用來記錄上一個節點倆倆相加後超出個位數的部分
+
+        while l1 is not None or l2 is not None or temp != 0:
+            l1_val, l2_val = 0, 0
+
+            if l1 is not None:
+                l1_val = l1.val
+                l1 = l1.next
+            if l2 is not None:
+                l2_val = l2.val
+                l2 = l2.next
+
+            total = l1_val + l2_val + temp
+            temp = total // 10
+
+            node = ListNode(total % 10) # 只取個位數
+            
+            curr.next = node
+            curr = curr.next
+
+        return dummy.next
