44fdfa686db0b2b81b0ebccf6e4f01cb01fd147d
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-11 17:22:14 -0700

    Update 1035_Uncrossed_Lines.py

--- python3/1035_Uncrossed_Lines.py
+++ python3/1035_Uncrossed_Lines.py
@@ -17,3 +17,25 @@ class Solution:
                     dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
         return dp[n][m]
+
+# 用滾動數組 (較快)
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        n = len(nums1)
+        m = len(nums2)
+
+        # 因為只需要用到 [i-1, j-1], [i-1, j] --> 叫 old, 所有的 dp[i-1] 用 old 取代
+        #              [i,   j-1], [i, j] --> 叫 new, 所有的 dp[i] 用 new 取代
+        # 所以可以用 rolling window
+        old = [0] * (m + 1)
+        new = [0] * (m + 1)
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    new[j] = 1 + old[j - 1]
+                else:
+                    new[j] = max(new[j - 1], old[j])
+            old = new[:] # 記得要把 new 拷貝回 old
+
+        return new[m]
