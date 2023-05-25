58c7b435ea62e5a2848ea37a4d329e6ba3f8fc9d
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-24 20:41:44 -0400

    Create 1280_Students_and Examinations.sql

---
+++ SQL/1280_Students_and Examinations.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT s.student_id,
+       s.student_name,
+       s2.subject_name,
+       COUNT(e.subject_name) AS attended_exams
+FROM Students AS s
+JOIN  Subjects AS s2
+LEFT JOIN Examinations AS e
+ON s.student_id = e.student_id AND
+   e.subject_name = s2.subject_name
+GROUP BY 1, 2, 3
+ORDER BY 1, 3
