166eb4e7c49531c7374a5d18b2a74f3231fa456c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-25 17:45:28 -0400

    Update 1141_User_Activity_for_the_Past_30_Days_I.sql

--- SQL/1141_User_Activity_for_the_Past_30_Days_I.sql
+++ SQL/1141_User_Activity_for_the_Past_30_Days_I.sql
@@ -18,3 +18,12 @@ FROM Activity
 WHERE activity_date >= '2019-06-28' AND activity_date <= '2019-07-27'
 GROUP BY 1
 ORDER BY 1;
+
+-- 
+# Write your MySQL query statement below
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+GROUP BY 1
