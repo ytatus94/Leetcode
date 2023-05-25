a9e6e8058aa44a7721f593a5ad1d37773dd9b5ed
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-25 15:33:12 -0400

    Update 1193_Monthly_Transactions_I.sql

--- SQL/1193_Monthly_Transactions_I.sql
+++ SQL/1193_Monthly_Transactions_I.sql
@@ -1,9 +1,23 @@
+# 方法 1.
 # Write your MySQL query statement below
 SELECT
     LEFT(trans_date, 7) AS month,
     country,
     COUNT(id) AS trans_count,
     SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM Transactions
+GROUP BY 1, 2
+ORDER BY 1, 2 DESC;
+
+# 方法 2.
+# Write your MySQL query statement below
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(id) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
     SUM(amount) AS trans_total_amount,
     SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
 FROM Transactions
