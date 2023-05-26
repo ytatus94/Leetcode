0fa370b750c97e4a813bf2d747845b290326b557
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 11:27:07 -0400

    Update 0619_Biggest_Single_Number.sql

--- SQL/0619_Biggest_Single_Number.sql
+++ SQL/0619_Biggest_Single_Number.sql
@@ -1,11 +1,15 @@
-SELECT MAX(num) AS num
-FROM (SELECT num
-      FROM number
-      GROUP BY num
-      HAVING COUNT(*) = 1) AS t;
+-- num 可能會有重複，要先找出不重複的 num，再從這些裡面找出最大的數值
 
-# 方法 2.
-# Write your MySQL query statement below
+-- 方法 1.
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM number
+    GROUP BY num
+    HAVING COUNT(*) = 1
+) AS t;
+
+-- 方法 2.
 SELECT
     MAX(t.num) AS num
 FROM (
@@ -16,4 +20,3 @@ FROM (
     GROUP BY 1
 ) AS t
 WHERE cnt = 1
-
