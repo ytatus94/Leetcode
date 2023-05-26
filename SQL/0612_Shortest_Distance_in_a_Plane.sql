87ef4a9959d65e3aa5dad267302322999640cd6a
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 11:23:22 -0400

    Update 0612_Shortest_Distance_in_a_Plane.sql

--- SQL/0612_Shortest_Distance_in_a_Plane.sql
+++ SQL/0612_Shortest_Distance_in_a_Plane.sql
@@ -5,4 +5,4 @@
 SELECT ROUND(SQRT(MIN(POWER(p1.x-p2.x, 2) + POWER(p1.y-p2.y, 2))), 2) AS shortest
 FROM point_2d AS p1
 JOIN point_2d AS p2
-ON p1.x != p2.x OR p1.y != p2.y
+ON p1.x != p2.x OR p1.y != p2.y;
