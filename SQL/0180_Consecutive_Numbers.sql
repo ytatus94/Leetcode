27c7e24820c8774c59247917e0e0a8c55b5c146c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-25 20:39:57 -0400

    Update 0180_Consecutive_Numbers.sql

--- SQL/0180_Consecutive_Numbers.sql
+++ SQL/0180_Consecutive_Numbers.sql
@@ -8,3 +8,11 @@ FROM Logs AS l1, Logs AS l2, Logs AS l3
 WHERE l1.id + 1 = l2.id AND l2.id + 1 = l3.id
 AND l1.num = l2.num AND l2.num = l3.num;
 
+# 方法 2.
+# Write your MySQL query statement below
+SELECT
+    DISTINCT l1.num AS ConsecutiveNums
+FROM Logs AS l1, Logs AS l2, Logs AS l3 
+WHERE
+    l1.id = l2.id - 1 AND l2.id = l3.id - 1 AND
+    l1.num = l2.num AND l2.num = l3.num;
