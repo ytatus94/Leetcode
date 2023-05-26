91a2f4bd52e408a74698e05940623c5a484f8d5c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-25 20:35:19 -0400

    Create 1789_Primary_Department_for_Each_Employee.sql

---
+++ SQL/1789_Primary_Department_for_Each_Employee.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+WHERE
+    primary_flag = 'Y' OR
+    employee_id IN (
+        SELECT employee_id
+        FROM Employee
+        GROUP BY 1
+        HAVING COUNT(employee_id) = 1
+);
