ac6e8bf876ed025be2ea6501c9c6ac2842fe0d35
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 10:29:18 -0400

    Update 0197_Rising_Temperature.sql

--- SQL/0197_Rising_Temperature.sql
+++ SQL/0197_Rising_Temperature.sql
@@ -1,12 +1,12 @@
 -- 考會不會使用 DATEDIFF() 函數
--- 方法 1
+-- 方法 1. w1 是第二天的, w2 是第一天的
 SELECT w1.Id
 FROM Weather AS w1
 JOIN Weather AS w2
 ON DATEDIFF(w1.RecordDate, w2.RecordDate) = 1
 WHERE w1.Temperature > w2.Temperature;
 
--- 方法 2
+-- 方法 2. w2 是第二天的, w1 是第一天的
 SELECT w2.id AS id
 FROM Weather w1, Weather w2
 WHERE DATEDIFF(w2.recordDate, w1.recordDate) = 1
