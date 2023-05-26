d50bd7111c7e32b0d2e1f3f8c064029caa87436c
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 10:35:34 -0400

    Update 0262_Trips_and_Users.sql

--- SQL/0262_Trips_and_Users.sql
+++ SQL/0262_Trips_and_Users.sql
@@ -2,6 +2,7 @@
 -- CASE 述句會產生一個新的欄位，當 status 有 cancelled 的話，新欄位的值就會是 1 沒有的話就是 0
 -- 然後 GROUP BY 時會把這個新欄位加起來，再除以總 row 數目 (用 COUNT 算出來)
 -- 最後用 ROUND() 取小數點後兩位數
+-- 注意題目要求是用 unbanned user 所以要先把 banned user 從 client_id 和 driver_id 中去除
 SELECT
     request_at AS Day,
     ROUND(SUM(CASE WHEN status LIKE 'cancelled%' THEN 1 ELSE 0 END) / COUNT(request_at), 2) AS 'Cancellation Rate'
