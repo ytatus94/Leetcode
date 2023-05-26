b1dae42229d88a8ded447a9ba3e0cf8bbbc33fd5
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 10:12:20 -0400

    Update 0175_Combine_Two_Tables.sql

--- SQL/0175_Combine_Two_Tables.sql
+++ SQL/0175_Combine_Two_Tables.sql
@@ -7,3 +7,7 @@ SELECT
 FROM Person
 LEFT JOIN Address
 ON Person.PersonID = Address.PersonId;
+
+-- 這題並不能用 FROM Person p, Address a WHERE p.personId = a.personId
+-- 因為這種寫法是 inner join, 結果只會是 Person 和 Address 的交集
+-- 要用 left join 才能保留全部的 p.personId
