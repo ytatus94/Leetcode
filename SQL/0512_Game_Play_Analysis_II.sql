633a96146715e3dfb6e88eec93740620b06ac142
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 10:36:32 -0400

    Update 0512_Game_Play_Analysis_II.sql

--- SQL/0512_Game_Play_Analysis_II.sql
+++ SQL/0512_Game_Play_Analysis_II.sql
@@ -1,8 +1,8 @@
 -- 先用 sub query 找出每個使用者最早登入的時間
 -- 再用 WHERE 篩選，並顯示該使用者在第一次登入時使用的設備
 SELECT
-  A1.player_id,
-  A1.device_id
+    A1.player_id,
+    A1.device_id
 FROM Activity A1
 WHERE (A1.player_id, A1.event_date) IN (
     SELECT
