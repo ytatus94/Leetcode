49756c5660c1e5f2310da84537c57eeed1e44bd8
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 10:58:24 -0400

    Update 0603_Consecutive_Available_Seats.sql

--- SQL/0603_Consecutive_Available_Seats.sql
+++ SQL/0603_Consecutive_Available_Seats.sql
@@ -1,5 +1,8 @@
+-- 座位要相鄰，所以編號差 1
+-- 考會不會用 ABS()
 SELECT DISTINCT c1.seat_id
 FROM cinema AS c1
 JOIN cinema AS c2
-WHERE ABS(c1.seat_id - c2.seat_id) = 1 AND (c1.free = 1 AND c2.free = 1)
+WHERE ABS(c1.seat_id - c2.seat_id) = 1
+AND (c1.free = 1 AND c2.free = 1)
 ORDER BY c1.seat_id;
