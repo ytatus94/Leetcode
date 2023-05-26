e2d7fc76c6222589577defbdbca443350427c270
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 11:24:42 -0400

    Update 0613_Shortest_Distance_in_a_Line.sql

--- SQL/0613_Shortest_Distance_in_a_Line.sql
+++ SQL/0613_Shortest_Distance_in_a_Line.sql
@@ -1,4 +1,7 @@
+-- 直線上的兩個點的最短距離，就用 |x1 - x2|
+-- 這題就是考會不會用 ABS()
+-- 注意這題要找出所有距離中最小值，所以要有 MIN()
 SELECT MIN(ABS(p1.x - p2.x)) AS shortest
 FROM point AS p1
 JOIN point AS p2
-WHERE p1.x != p2.x
+WHERE p1.x != p2.x;
