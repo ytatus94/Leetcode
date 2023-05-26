d579fc954bd515d50c1c94ccd537a0f3fe251c46
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 10:56:16 -0400

    Update 0597_Friend_Requests_I_Overall_Acceptance_Rate.sql

--- SQL/0597_Friend_Requests_I_Overall_Acceptance_Rate.sql
+++ SQL/0597_Friend_Requests_I_Overall_Acceptance_Rate.sql
@@ -1,3 +1,6 @@
+-- IFNULL(expression, alt_value) 判斷 expression 是不是 NULL
+-- 如果不是, 傳回 expression 的結果
+-- 如果是, 傳回 alt_value
 SELECT IFNULL(ROUND((SELECT COUNT(DISTINCT requester_id, accepter_id)
                      FROM request_accepted) /
                     (SELECT COUNT(DISTINCT sender_id, send_to_id)
