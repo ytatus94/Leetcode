56bb90aef757ed97ed9a6da4e8a42fc6b7696803
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 11:21:15 -0400

    Update 0612_Shortest_Distance_in_a_Plane.sql

--- SQL/0612_Shortest_Distance_in_a_Plane.sql
+++ SQL/0612_Shortest_Distance_in_a_Plane.sql
@@ -1,4 +1,8 @@
+-- 兩點之間的最短距離就是直線
+-- distance = sqrt( (x1 - x2)^2 + (y1 - y2)^2 )
+-- 這一題就是考會不會用 POWER(), SQRT() 等函數
+-- 還有要找的是全部的點裡面的最短距離，所以要記得加上 MIN()
 SELECT ROUND(SQRT(MIN(POWER(p1.x-p2.x, 2) + POWER(p1.y-p2.y, 2))), 2) AS shortest
 FROM point_2d AS p1
 JOIN point_2d AS p2
-ON p1.x != p2.y OR p1.y != p2.y
+ON p1.x != p2.x OR p1.y != p2.y
