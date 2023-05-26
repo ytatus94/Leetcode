2bc23e9716f66d2694eb5c79b768c33eb71e2985
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-26 11:02:17 -0400

    Update 0608_Tree_Node.sql

--- SQL/0608_Tree_Node.sql
+++ SQL/0608_Tree_Node.sql
@@ -2,14 +2,6 @@
 -- 當節點是 parent id 的時候，那一定是 inner
 -- 節點不是 parent id 的時候才是 leaf
 
-SELECT id, CASE
-               WHEN p_id IS NULL THEN 'Root'
-               WHEN id IN (SELECT DISTINCT p_id FROM tree) THEN 'Inner'
-               ELSE 'Leaf'
-           END AS 'Type'
-FROM tree
-
--- 這個比較好，有把 p_id IS NOT NULL 考慮進去
 SELECT
     id,
     CASE
