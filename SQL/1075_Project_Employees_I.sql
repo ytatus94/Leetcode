a6b3b5df5b3bfdcc039c3d03b217b664ef25d156
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-27 11:05:31 -0400

    Update 1075_Project_Employees_I.sql

--- SQL/1075_Project_Employees_I.sql
+++ SQL/1075_Project_Employees_I.sql
@@ -3,4 +3,4 @@ SELECT project_id,
        ROUND(SUM(e.experience_years) / COUNT(e.employee_id), 2) average_years
 FROM Project AS p, Employee AS e
 WHERE p.employee_id = e.employee_id
-GROUP BY 1
+GROUP BY 1;
