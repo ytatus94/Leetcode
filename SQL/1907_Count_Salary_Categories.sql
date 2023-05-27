1bdbdda960a0222779e31e81999d578cfb2d396b
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-27 11:25:37 -0400

    Update 1907_Count_Salary_Categories.sql

--- SQL/1907_Count_Salary_Categories.sql
+++ SQL/1907_Count_Salary_Categories.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+-- 分開成 3 種情況討論，再用 UNION 結合起來
 SELECT 
     'High Salary' AS category,
     COUNT(*) AS accounts_count
@@ -16,3 +16,7 @@ SELECT
     COUNT(*) AS accounts_count
 FROM Accounts
 WHERE income BETWEEN 20000 AND 50000;
+
+-- 這題如果用 CASE 子句產生 category 再 group by 起來，計算數目的話
+-- 因為表格內沒有符合 Average Salary 的紀錄，group by 之後表格內不會有 Average Salary 的結果
+-- 所以不能用這個方式
