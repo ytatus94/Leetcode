3027cc0431240afcb9c4a8a72f26dc74e3878c91
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2023-05-27 12:10:47 -0400

    Update 0626_Exchange_Seats.sql

--- SQL/0626_Exchange_Seats.sql
+++ SQL/0626_Exchange_Seats.sql
@@ -1,3 +1,4 @@
+-- 方法 1. 用 CASE
 SELECT
     CASE
         WHEN MOD(id, 2) = 1 AND id = counts THEN id
@@ -7,4 +8,14 @@ SELECT
     student
 FROM seat, (SELECT COUNT(*) AS counts
             FROM seat) AS seat_counts
+ORDER BY id ASC;
+
+-- 方法 2. 用 IF
+-- 如果最後一個 id 是奇數, 就不變 (id = id)
+-- 如果 id 是奇數, 就 id = id+1
+-- 如果 id 是偶數, 就 id = id-1
+SELECT
+    IF(id % 2 = 1, IF(id = (SELECT COUNT(*) FROM Seat), id, id + 1), id - 1) AS id,
+    student
+FROM Seat
 ORDER BY id ASC;
